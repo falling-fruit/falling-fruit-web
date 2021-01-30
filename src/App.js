a249841e933710a21ddc8f110696c0112ae59556
Jeffrey Tang <jt12@illinois.edu>
2021-01-29 23:24:57 -0600

    🚀 Setup routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ExamplePage from './pages/ExamplePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <ExamplePage />
+      </Route>
+      <Route>Not found</Route>
+    </Switch>
+  </Router>
+)
 
 export default App
---
+++ src/pages/ExamplePage.js
@@ -0,0 +1,17 @@
+const ExamplePage = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>Edit src/App.js and save to reload.</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+)
+
+export default ExamplePage
