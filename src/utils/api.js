3cd351c5beb2a6d0f587f19a58d12cfbff99f3ff
Vasu Chalasani <sc83@illinois.edu>
2021-02-13 21:08:53 -0600

    add getLocation functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -27,12 +27,9 @@ const handleResponse = (request) =>
   )
 
 export const getClusters = (
-  swlng,
-  nelng,
-  swlat,
-  nelat,
-  zoom = null,
-  muni = null,
+  { swlng, nelng, swlat, nelat },
+  zoom = 0,
+  muni = false,
   types = null,
 ) =>
   handleResponse(
@@ -43,10 +40,90 @@ export const getClusters = (
         swlat,
         nelat,
         zoom,
-        muni,
+        muni: muni ? 1 : 0,
         t: types,
       },
     }),
   )
+
+export const LOCALE = {
+  EN: 'en',
+  DE: 'de',
+  EL: 'el',
+  ES: 'es',
+  FR: 'fr',
+  HE: 'he',
+  IT: 'it',
+  NL: 'nl',
+  PL: 'pl',
+}
+
+export const getLocation = (
+  { swlng, nelng, swlat, nelat },
+  includeMunicipal = false,
+  categories = null,
+  types = null,
+  locale = LOCALE.EN,
+  invasive = false,
+  limit = 1000,
+  offset = 0,
+  reviews = false,
+  { lng = 0, lat = 0 },
+) =>
+  handleResponse(
+    instance.get('/locations.json', {
+      params: {
+        swlng,
+        nelng,
+        swlat,
+        nelat,
+        municipal: includeMunicipal ? 1 : 0,
+        c: categories,
+        t: types,
+        invasive: invasive ? 1 : 0,
+        limit,
+        offset,
+        locale,
+        reviews: reviews ? 1 : 0,
+        lng,
+        lat,
+      },
+    }),
+  )
+
+// export const postLocation = (
+//   {lng, lat},
+//   type_ids = null,
+//   includeMunicipal = false,
+//   c = null,
+//   t = null,
+//   locale,
+//   invasive = false,
+//   limit = 1000,
+//   offset = 0,
+//   reviews = false,
+//   lng = 0,
+//   lat = 0
+// ) =>
+//   handleResponse(
+//     instance.post('/locations.json', photo_data, {
+//       params: {
+//         swlng,
+//         nelng,
+//         swlat,
+//         nelat,
+//         municipal: includeMunicipal ? 1 : 0,
+//         c: categories,
+//         t: types,
+//         invasive: invasive ? 1 : 0,
+//         limit,
+//         offset,
+//         locale,
+//         reviews: reviews ? 1 : 0,
+//         lng,
+//         lat
+//       },
+//     }),
+//   )
 
 export const getTypes = () => handleResponse(instance.get('/types.json', {}))
