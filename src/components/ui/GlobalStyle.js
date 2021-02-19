af40c8c51fe80b35e880dcc4cbdb68bac37a4b7c
Siraj <sirajchokshi@gmail.com>
2021-02-18 20:37:33 -0600

    add theme colors

--- src/components/ui/GlobalStyle.js
+++ src/components/ui/GlobalStyle.js
@@ -7,7 +7,17 @@ const GlobalStyle = createGlobalStyle`
 `
 
 const theme = {
-  textColor: '#9B9B9B',
+  // text colors
+  text: '#9B9B9B',
+  secondaryText: '#5A5A5A',
+  tertiaryText: '#979797',
+  // background
+  background: '#FFFFFF',
+  secondaryBackground: '#E0E1E2',
+  // palette
+  orange: '#FFA41B',
+  blue: '#4183C4',
+  green: '#73CD7C',
 }
 
 export default GlobalStyle
