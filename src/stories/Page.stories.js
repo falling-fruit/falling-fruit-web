3a4ec0ca8af9857575b935d8cf13079e144aa912
Jeffrey Tang <jt12@illinois.edu>
2021-02-18 20:19:57 -0600

    Add storybook template

---
+++ src/stories/Page.stories.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import * as HeaderStories from './Header.stories'
+import { Page } from './Page'
+
+export default {
+  title: 'Example/Page',
+  component: Page,
+}
+
+const Template = (args) => <Page {...args} />
+
+export const LoggedIn = Template.bind({})
+LoggedIn.args = {
+  ...HeaderStories.LoggedIn.args,
+}
+
+export const LoggedOut = Template.bind({})
+LoggedOut.args = {
+  ...HeaderStories.LoggedOut.args,
+}
