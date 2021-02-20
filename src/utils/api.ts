46d40c195c52e6752344d54dcc7009bd45c6b7fc
Vasu Chalasani <sc83@illinois.edu>
2021-02-19 21:14:10 -0600

    delete extra multiline comment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -64,20 +64,6 @@ export const postLocations = (
   )
 }
   
-
-  // You will want to take photo_data in the same way you take id, with a few differences:
-  // 1. Type it as photo_data?: File (the ? makes it optional)
-  // 2. Send it as multi-part form data. See here: https://stackoverflow.com/a/43014086/2411756
-  //    (for formData.append, use "photo_data" for the 1st argument, and the actual argument photo_data for the 2nd argument)
-  // 3. Make sure you pass the correct headers in Axios!
-  // 4. Pass formData directly as the 2nd argument of instance.post. However, if (!photo_data), pass null instead!
-  // If you're having trouble, please read through these links:
-  // - https://developer.mozilla.org/en-US/docs/Web/API/FormData
-  // - https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
-  // You will need to give this function an actual body, i.e. add braces after => and return handleResponse after
-  // you finish building the formData object.
-
-// Follow this example!
 export const getLocationById = (
   id: paths['/locations/{id}.json']['get']['parameters']['path']['id'],
   params: paths['/locations/{id}.json']['get']['parameters']['query'],
