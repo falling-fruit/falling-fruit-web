463974315bf7b8f70480f34ce32ce8eb104081b1
Archna-1 <archnas2@illinois.edu>
2021-02-20 14:13:09 -0600

    Rewrite postReview in api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -185,38 +185,28 @@ export const getTypesById = (
   )
 
 export const getReviews = (
-  id: paths['/locations/{id}/reviews.json']['get']['parameters']['path']['id'],
+  locationId: paths['/locations/{id}/reviews.json']['get']['parameters']['path']['id'],
 ) =>
   handleResponse(
-    instance.get(`/locations/${id}/reviews.json`)
+    instance.get(`/locations/${locationId}/reviews.json`)
   )
-
+  
+  var imagefile = document.querySelector('#file');
 export const postReview = (
+  params: paths['/locations/{id}/review.json']['post']['parameters']['query'],
   id: paths['/locations/{id}/review.json']['post']['parameters']['path']['id'],
-  photo_file_name = null,
-  // You will want to take photo_data in the same way you take id, with a few differences:
-  // 1. Type it as photo_data?: File (the ? makes it optional)
-  // 2. Send it as multi-part form data. See here: https://stackoverflow.com/a/43014086/2411756
-  //    (for formData.append, use "photo_data" for the 1st argument, and the actual argument photo_data for the 2nd argument)
-  // 3. Make sure you pass the correct headers in Axios!
-  // 4. Pass formData directly as the 2nd argument of instance.post. However, if (!photo_data), pass null instead!
-  // If you're having trouble, please read through these links:
-  // - https://developer.mozilla.org/en-US/docs/Web/API/FormData
-  // - https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
-  // You will need to give this function an actual body, i.e. add braces after => and return handleResponse after
-  // you finish building the formData object.
-  photo_data
-) =>
-  handleResponse(
-    instance.post(`/locations/${id}/review.json`, photo_data, {
-      params: {
-        author,
-        comment,
-        fruiting,
-        quality_rating,
-        yield_rating,
-        observed_on,
-        photo_file_name,
+  photo_data?: File
+) => {
+  var formData = new FormData();
+
+  formData.append("photo_data", photo_data);
+  
+  return handleResponse(
+    instance.post(`/locations/${id}/review.json`, formData != null ? formData : null, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
       },
+      params,
     }),
   )
+}
