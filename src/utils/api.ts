a6b53e113ccd2e80158ce8edaf34d88694fd6319
Vasu Chalasani <sc83@illinois.edu>
2021-02-19 21:13:05 -0600

    rewrote new param objects

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -28,21 +28,6 @@ const handleResponse = (request: Promise<AxiosResponse<any>>) =>
     },
   )
 
-// Since all of the locale values are in the TypeScript schema now, we don't
-// need this "enum" anymore. Please delete it!
-export const LOCALE = {
-  EN: 'en',
-  DE: 'de',
-  EL: 'el',
-  ES: 'es',
-  FR: 'fr',
-  HE: 'he',
-  IT: 'it',
-  NL: 'nl',
-  PL: 'pl',
-}
-Object.freeze(LOCALE)
-
 // Follow this example!
 export const getClusters = (
   params: paths['/clusters.json']['get']['parameters']['query'],
@@ -54,79 +39,43 @@ export const getClusters = (
   )
 
 export const getLocations = (
-  { swlng, nelng, swlat, nelat },
-  includeMunicipal = false,
-  categories = null,
-  types = null,
-  locale = LOCALE.EN,
-  invasive = false,
-  limit = 1000,
-  offset = 0,
-  reviews = false,
-  { lng = 0, lat = 0 },
+  params: paths['/locations.json']['get']['parameters']['query'],
 ) =>
   handleResponse(
     instance.get('/locations.json', {
-      params: {
-        swlng,
-        nelng,
-        swlat,
-        nelat,
-        muni: includeMunicipal ? 1 : 0,
-        c: categories,
-        t: types,
-        invasive: invasive ? 1 : 0,
-        limit,
-        offset,
-        locale,
-        reviews: reviews ? 1 : 0,
-        lng,
-        lat,
-      },
+      params,
     }),
-  )
+  )  
+
 
 export const postLocations = (
-  { lng, lat },
-  type_ids,
-  author = null,
-  description = null,
-  season_start = 0,
-  season_stop = 0,
-  no_season = false,
-  unverified = false,
-  access = 0,
-  comment = null,
-  fruiting = 0,
-  quality_rating = 0,
-  yield_rating = 0,
-  observed_on = null,
-  photo_file_name = null,
-  // See below for how to handle photo_data.
-  { photo_data } = null,
-) =>
-  handleResponse(
+  params: paths['/locations.json']['post']['parameters']['query'],
+  photo_data?: File['/locations.json']['post']['parameters']['query'] ,
+) => {
+  var formData = new FormData();
+  formData.append("photo_data", photo_data)
+  return handleResponse(
     instance.post('/locations.json', photo_data, {
-      params: {
-        lng,
-        lat,
-        type_ids,
-        author,
-        description,
-        season_start,
-        season_stop,
-        no_season,
-        unverified,
-        access,
-        comment,
-        fruiting,
-        quality_rating,
-        yield_rating,
-        observed_on,
-        photo_file_name,
+      headers: {
+        'Content-Type': 'multipart/form-data'
       },
+      params,
     }),
   )
+}
+  
+
+  // You will want to take photo_data in the same way you take id, with a few differences:
+  // 1. Type it as photo_data?: File (the ? makes it optional)
+  // 2. Send it as multi-part form data. See here: https://stackoverflow.com/a/43014086/2411756
+  //    (for formData.append, use "photo_data" for the 1st argument, and the actual argument photo_data for the 2nd argument)
+  // 3. Make sure you pass the correct headers in Axios!
+  // 4. Pass formData directly as the 2nd argument of instance.post. However, if (!photo_data), pass null instead!
+  // If you're having trouble, please read through these links:
+  // - https://developer.mozilla.org/en-US/docs/Web/API/FormData
+  // - https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
+  // You will need to give this function an actual body, i.e. add braces after => and return handleResponse after
+  // you finish building the formData object.
 
 // Follow this example!
 export const getLocationById = (
@@ -140,59 +89,63 @@ export const getLocationById = (
   )
 
 export const editLocation = (
-  id,
-  { lng, lat },
-  type_ids,
-  author = null,
-  description = null,
-  season_start = 0,
-  season_stop = 0,
-  no_season = false,
-  unverified = false,
-  access = 0,
+  id: paths['/locations/{id}.json']['post']['parameters']['path']['id'],
+  params: paths['/locations/{id}.json']['post']['parameters']['query'],
 ) =>
   handleResponse(
     instance.post(`/locations/${id}.json`, null, {
-      params: {
-        lng,
-        lat,
-        type_ids,
-        author,
-        description,
-        season_start,
-        season_stop,
-        no_season,
-        unverified,
-        access,
-      },
-    }),
-  )
-
-export const getTypes = (
-  params: paths['/types.json']['get']['parameters']['query'],
-) =>
-  handleResponse(
-    instance.get('/types.json', {
       params,
     }),
   )
 
-export const getTypesById = (
-  id: paths['/types/{id}.json']['get']['parameters']['path']['id'],
+export const getTypes = (
+  { swlng, nelng, swlat, nelat },
+  zoom = 0,
+  muni = false,
+  locale = LOCALE.EN,
+  types = null,
+  urls = false,
+  categories = null,
+  uncategorized = false,
+  pending = false,
 ) =>
   handleResponse(
-    instance.get(`/types/${id}.json`)
+    instance.get('/types.json', {
+      params: {
+        swlng,
+        nelng,
+        swlat,
+        nelat,
+        zoom,
+        muni: muni ? 1 : 0,
+        locale,
+        t: types,
+        urls: urls ? 1 : 0,
+        categories,
+        uncategorized: uncategorized ? 1 : 0,
+        pending: pending ? 1 : 0,
+      },
+    }),
   )
 
-export const getReviews = (
-  id: paths['/locations/{id}/reviews.json']['get']['parameters']['path']['id'],
-) =>
+export const getTypesById = (id) =>
+  handleResponse(instance.get(`/types/${id}.json`))
+
+export const getReviews = (id) =>
   handleResponse(
-    instance.get(`/locations/${id}/reviews.json`)
+    instance.get(`/locations/${id}/reviews.json`, {
+      params: {},
+    }),
   )
 
 export const postReview = (
-  id: paths['/locations/{id}/review.json']['post']['parameters']['path']['id'],
+  id,
+  author = null,
+  comment = null,
+  fruiting = null,
+  quality_rating = null,
+  yield_rating = null,
+  observed_on = null,
   photo_file_name = null,
   // You will want to take photo_data in the same way you take id, with a few differences:
   // 1. Type it as photo_data?: File (the ? makes it optional)
@@ -205,7 +158,7 @@ export const postReview = (
   // - https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
   // You will need to give this function an actual body, i.e. add braces after => and return handleResponse after
   // you finish building the formData object.
-  photo_data
+  { photo_data },
 ) =>
   handleResponse(
     instance.post(`/locations/${id}/review.json`, photo_data, {
