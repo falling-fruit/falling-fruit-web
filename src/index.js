0e6cc88855465e80a9bfa18df4a2a7c1f046a7a8
Jeffrey Tang <jt12@illinois.edu>
2021-02-21 23:16:48 -0600

    🧹 Move reportWebVitals to utils

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import reportWebVitals from './utils/reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
