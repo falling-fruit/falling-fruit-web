ee99c9cd83e1d17c3f0b56b75d57c1aa3414e6c0
Jeffrey Tang <jt12@illinois.edu>
2021-02-22 01:44:03 -0600

    Route to desktop and mobile pages

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle, { theme } from './components/ui/GlobalStyle'
+import DesktopLayout from './pages/desktop/DesktopLayout'
 import MainPage from './pages/MainPage'
-import MapPage from './pages/MapPage'
 import MobileLayout from './pages/mobile/MobileLayout'
-import Types from './pages/Types'
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <Router>
       <Switch>
-        <Route exact path="/types">
-          <Types />
+        <Route path="/desktop">
+          <DesktopLayout />
         </Route>
-        <Route exact path="/map">
-          <MapPage />
-        </Route>
-        <Route exact path="/mobile">
+        <Route path="/mobile">
           <MobileLayout />
         </Route>
-        <Route exact path="/">
+        <Route path="/">
           <MainPage />
         </Route>
         <Route>Not found</Route>
