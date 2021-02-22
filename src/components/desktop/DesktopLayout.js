325bbe82102111e6105e906b44fe7c08e144a2c4
Jeffrey Tang <jt12@illinois.edu>
2021-02-22 02:39:37 -0600

    Add left-side pane for desktop

--- src/components/desktop/DesktopLayout.js
+++ src/components/desktop/DesktopLayout.js
@@ -2,19 +2,24 @@ import styled from 'styled-components'
 
 import MapPage from '../map/MapPage'
 import Header from './Header'
+import Pane from './Pane'
 
 const DesktopContainer = styled.div`
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `
 
 const MapContainer = styled.div`
-  height: 100%;
+  position: relative;
+  flex: 1;
 `
 
 const DesktopLayout = () => (
   <DesktopContainer>
     <Header />
     <MapContainer>
+      <Pane />
       <MapPage />
     </MapContainer>
   </DesktopContainer>
