68ddc8e91f334dc64bac802218f4dc92a500253b
Jeffrey Tang <jt12@illinois.edu>
2021-02-24 20:01:49 -0600

    🪟 Add resizable pane to desktop layout (#20)
    * Install @reach/window-size and peer deps
    
    * Add desktop/mobile breakpoint components
    
    * Pin tabs to bottom of screen
    
    * Add desktop/mobile layout stubs
    
    * Remove unused page
    
    * Route to desktop and mobile pages
    
    * Add header to desktop breakpoint
    
    * Remove example page
    
    * Move all pages into components
    
    * Add left-side pane for desktop
    
    * Add resizable pane to desktop layout
    
    * Adjust default, min, and max width for pane
    
    * Adjust max width to default width
    
    * Use custom hooks for breakpoints
    
    * Make map not take up minWidth space of pane
    
    * Rename api-schema to apiSchema
    
    * Change to side panel instead of overlay
    
    * Remove unused dependency
    
    * Fix mobile viewport issues (caused by 100vh)
    
    * Use constants for min pane width

--- src/components/desktop/DesktopLayout.js
+++ src/components/desktop/DesktopLayout.js
@@ -1,22 +1,58 @@
+import WindowSize from '@reach/window-size'
+import SplitPane from 'react-split-pane'
 import styled from 'styled-components'
 
 import MapPage from '../map/MapPage'
 import Header from './Header'
+import SidePane from './SidePane'
+
+// Min and max pane width, in pixels
+const MIN_PANE_WIDTH = () => 310 // eslint-disable-line no-magic-numbers
+const MAX_PANE_WIDTH = (vw) => (21.5 * vw) / 100 // eslint-disable-line no-magic-numbers
 
 const DesktopContainer = styled.div`
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `
 
-const MapContainer = styled.div`
+const StyledSplit = styled(SplitPane)`
+  position: relative !important;
+  flex: 1;
+
+  .Resizer {
+    width: 10px;
+    cursor: col-resize;
+    z-index: 1;
+    box-shadow: 8px 0px 8px -4px rgba(0, 0, 0, 0.12);
+  }
+`
+
+const MapPane = styled.div`
+  width: 100%;
   height: 100%;
 `
 
 const DesktopLayout = () => (
+  // Hack: must use WindowSize here because react-split-pane doesn't allow for
+  // a non-numerical maxSize like "21.5vw"
   <DesktopContainer>
     <Header />
-    <MapContainer>
-      <MapPage />
-    </MapContainer>
+    <WindowSize>
+      {({ width: vw }) => (
+        <StyledSplit
+          split="vertical"
+          minSize={MIN_PANE_WIDTH(vw)}
+          maxSize={MAX_PANE_WIDTH(vw)}
+          defaultSize={MAX_PANE_WIDTH(vw)}
+        >
+          <SidePane />
+          <MapPane>
+            <MapPage />
+          </MapPane>
+        </StyledSplit>
+      )}
+    </WindowSize>
   </DesktopContainer>
 )
 
