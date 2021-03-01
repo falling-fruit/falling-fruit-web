99f2c27faf1eb4ccaec86e8ce82b5eedd4594fc3
code-arman <armanr123@gmail.com>
2021-03-01 11:49:20 -0600

    Add styles to tooltip popup

--- src/components/ui/MapTooltip.js
+++ src/components/ui/MapTooltip.js
@@ -1,7 +1,7 @@
 // import PropTypes from 'prop-types'
 import '@reach/tooltip/styles.css'
 
-import TooltipPopup from '@reach/tooltip'
+import { Tooltip } from '@reach/tooltip'
 import styled from 'styled-components'
 
 const ListEntryWrapper = styled.div`
@@ -12,11 +12,22 @@ const ListEntryWrapper = styled.div`
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.12);
   border-radius: 44px;
 `
+const StyledTooltip = styled(Tooltip)`
+  background-color: #fff;
+  border: none;
+  box-shadow: none;
+
+  ${'' /* & > * {
+    background-color: #FFF;
+  } */}
+`
 
 const MapTooltip = ({ ListEntry }) => (
-  <TooltipPopup>
-    <ListEntryWrapper> {ListEntry} </ListEntryWrapper>
-  </TooltipPopup>
+  <div>
+    <StyledTooltip label={<ListEntryWrapper> {ListEntry} </ListEntryWrapper>}>
+      <button style={{ fontSize: 25 }}>Location</button>
+    </StyledTooltip>
+  </div>
 )
 
 export default MapTooltip
