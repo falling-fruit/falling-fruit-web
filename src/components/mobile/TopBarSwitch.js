5e3ab96e558786526534b4393baa648944210dce
Jeffrey Tang <jt12@illinois.edu>
2021-03-01 18:05:24 -0600

    Add router switch to top bar

--- src/components/mobile/TopBarSwitch.js
+++ src/components/mobile/TopBarSwitch.js
@@ -1,10 +1,22 @@
+import { Route, Switch } from 'react-router-dom'
+
 import Search from '../search/Search'
 import TopBar from '../ui/TopBar'
 
 const TopBarSwitch = () => (
-  <TopBar>
-    <Search />
-  </TopBar>
+  <Switch>
+    <Route path="/map">
+      <TopBar>
+        <Search />
+      </TopBar>
+    </Route>
+    <Route path="/list">
+      <TopBar>
+        <Search />
+      </TopBar>
+    </Route>
+    <Route></Route>
+  </Switch>
 )
 
 export default TopBarSwitch
