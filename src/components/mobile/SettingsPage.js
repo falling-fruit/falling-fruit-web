1a522f9d68ec0b1c05010fa78edc08ad885d7030
code-arman <46976119+code-arman@users.noreply.github.com>
2021-03-03 20:18:00 -0600

    🗒️  Add list component (#26)
    * Add Arman photo
    
    * Add initial ListEntry component
    
    * Improve styling of ListEntry
    
    * Add sample images to ListEntry
    
    * Add prop types to ListEntry
    
    * Clean up code style
    
    * Remove explicit margins
    
    * Remove extra css
    
    * Remove comments
    
    * Remove repetition of align-self
    
    * Add minor CSS adjustment
    
    * Add styled MapTooltip componenet
    
    * Add styles to tooltip popup
    
    * Add triangle tooltip
    
    * Remove dropshadow from tooltip
    
    * Add TooltipWrapper
    
    * Remove MapTooltip
    
    * Add more examples of component on screen
    
    * Make both icons same size
    
    * Fix centering of icons
    
    * Add vertical centering to icons
    
    * Remove extra files for review
    
    * Readd rightIcon propTypes
    
    * Remove extra dependencies
    
    * Add bottom-border to ListEntry
    
    * Add support for multiple icons passed into ListEntry
    
    * Add border to all entries except last
    
    * Add iconSize parameter to ListEntry
    
    * Revert "Add iconSize parameter to ListEntry"
    
    This reverts commit 87a109f5e4a33887b68b8b2af94a1c24932d02cb.
    
    * Update yarn.lock
    
    Co-authored-by: Archna-1 <archnas2@illinois.edu>
    Co-authored-by: Jeffrey Tang <jt12@illinois.edu>

--- src/components/mobile/SettingsPage.js
+++ src/components/mobile/SettingsPage.js
@@ -3,7 +3,10 @@ import { Flag, Star } from '@styled-icons/boxicons-solid'
 
 import Button from '../ui/Button'
 import Input from '../ui/Input'
+import ListEntry from '../ui/ListEntry'
 import { Tag, TagList } from '../ui/Tag'
+
+const TwoIcons = [<Star size="16" key={1} />, <Star size="16" key={2} />]
 
 const SettingsPage = () => (
   <>
@@ -33,6 +36,18 @@ const SettingsPage = () => (
       ))}
     </TagList>
     <br />
+    <ListEntry
+      leftIcons={<Star size="16" />}
+      primaryText="Entry"
+      secondaryText="0.3 miles"
+    />
+    <ListEntry
+      primaryText="Entry"
+      secondaryText="0.3 miles"
+      rightIcons={TwoIcons}
+      size={57}
+    />
+    <ListEntry primaryText="Entry" rightIcons={<Star size="16" />} />
   </>
 )
 
---
+++ src/components/ui/ListEntry.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Icons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 36px;
+  & > * {
+    padding: 8px;
+  }
+`
+const PrimaryText = styled.div`
+  align-items: center;
+  font-weight: bold;
+  font-size: 14px;
+`
+const SecondaryText = styled.div`
+  align-items: center;
+  font-weight: normal;
+  font-size: 12px;
+`
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0 22px;
+  height: 57px;
+  align-items: center;
+  &:not(:last-child) {
+    border-bottom: 1px solid ${({ theme }) => theme.secondaryBackground};
+  }
+`
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.secondaryText};
+  justify-content: center;
+  margin-left: 18px;
+  flex: 1;
+`
+
+const ListEntry = ({ leftIcons, primaryText, secondaryText, rightIcons }) => (
+  <ListContainer>
+    <Icons>{leftIcons}</Icons>
+    <TextContainer>
+      <PrimaryText>{primaryText}</PrimaryText>
+      <SecondaryText>{secondaryText}</SecondaryText>
+    </TextContainer>
+    <Icons>{rightIcons}</Icons>
+  </ListContainer>
+)
+
+ListEntry.propTypes = {
+  leftIcons: PropTypes.node,
+  primaryText: PropTypes.string,
+  secondaryText: PropTypes.string,
+  rightIcons: PropTypes.node,
+}
+
+export default ListEntry
