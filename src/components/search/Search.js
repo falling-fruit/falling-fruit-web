7122530bda856452f466584aed6130636429beb9
Vasu Chalasani <sc83@illinois.edu>
2021-03-05 15:21:43 -0600

    use GeoCode to extract coords from address

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -9,18 +9,33 @@ import {
   ComboboxPopover,
 } from '@reach/combobox'
 import { SearchAlt2 } from '@styled-icons/boxicons-regular'
+import Geocode from 'react-geocode'
 import { useHistory } from 'react-router-dom'
 import usePlacesAutocomplete from 'use-places-autocomplete'
 
 import Input from '../ui/Input'
 
 const Search = () => {
+  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+  // const [swlng, setSwlng] = useState(0);
+  // const [swlat, setSwlat] = useState(0)
+  // const [nelng, setNelng] = useState(0)
+  // const [nelat, setNelat] = useState(0)
   let history = useHistory()
 
   const onSelectHandler = (item) => {
+    console.log(item)
+    getLongitudeAndLatitudeFromAddress()
     history.push({
       pathname: '/map',
       search: `?query=${item}`,
+    })
+  }
+
+  const getLongitudeAndLatitudeFromAddress = () => {
+    Geocode.fromAddress('Eiffel Tower').then((response) => {
+      const { lat, lng } = response.results[0].geometry.location
+      console.log(lat, lng)
     })
   }
 
@@ -43,7 +58,12 @@ const Search = () => {
   />
   return (
     <Combobox onSelect={onSelectHandler} aria-labelledby="demo">
-      <ComboboxInput value={value} onChange={handleInput} disabled={!ready} />
+      <ComboboxInput
+        as={Input}
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+      />
       <ComboboxPopover>
         <ComboboxList>
           {status === 'OK' &&
