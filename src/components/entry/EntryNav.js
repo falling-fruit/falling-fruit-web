99029f18249a9ecfaa9e2e95d9e85e2db93a0462
laurenho025 <laurenh4@illinois.edu>
2021-03-06 13:43:52 -0600

    Initial icon button implementation

--- src/components/entry/EntryNav.js
+++ src/components/entry/EntryNav.js
@@ -1,7 +1,33 @@
-const EntryNav = () => (
-  <>
-    <p>EntryNav</p>
-  </>
-)
+import { Pencil } from '@styled-icons/boxicons-solid'
+import { MapMarkerAlt as Map } from '@styled-icons/fa-solid'
+
+import { theme } from '../ui/GlobalStyle'
+import IconButton from '../ui/IconButton'
+
+const EntryNav = () => {
+  const onIconButtonClick = () => {
+    console.log('Icon button clicked')
+  }
+
+  return (
+    <>
+      <p>EntryNav</p>
+      <IconButton
+        size={50}
+        raised={false}
+        icon={<Pencil size="20" color={theme.secondaryText} />}
+        onClick={onIconButtonClick}
+        label={'edit-entry-details'}
+      />
+      <IconButton
+        size={50}
+        raised={false}
+        icon={<Map size="20" color={theme.secondaryText} />}
+        onClick={onIconButtonClick}
+        label={'map-entry-details'}
+      />
+    </>
+  )
+}
 
 export default EntryNav
