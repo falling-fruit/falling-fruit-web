af513858fcbc6614cb41fb25ac40ddc596d926b4
code-arman <armanr123@gmail.com>
2021-03-06 18:00:08 -0600

    Add left and right icons to SearchEntry

--- src/components/search/SearchEntry.js
+++ src/components/search/SearchEntry.js
@@ -1,9 +1,36 @@
-import ListEntry from '../ui/ListEntry'
+import { CursorFill } from '@styled-icons/bootstrap'
+import { Map } from '@styled-icons/boxicons-solid'
 
+import ListEntry from '../ui/ListEntry'
 const SearchEntry = ({ children }) => {
   const [primaryText, secondaryText] = children
 
-  return <ListEntry primaryText={primaryText} secondaryText={secondaryText} />
+  const leftIcon = (
+    <div
+      style={{
+        background: '#5A5A5A',
+        width: '20px',
+        height: '20px',
+        borderRadius: '50%',
+      }}
+    >
+      <Map size={20} color={'white'} />
+    </div>
+  )
+  const rightIcon = (
+    <div style={{ width: '20px' }}>
+      <CursorFill size={20} color={'#4183C4'} />
+    </div>
+  )
+
+  return (
+    <ListEntry
+      leftIcons={leftIcon}
+      primaryText={primaryText}
+      secondaryText={secondaryText}
+      rightIcons={rightIcon}
+    />
+  )
 }
 
 export default SearchEntry
