ce4c7b03f27124fc2596b86965f8e4d2582410bc
Vasudha Chalasani <40867028+vasuchal@users.noreply.github.com>
2021-03-10 20:46:00 -0600

    🔎 Location search (#34)
    * create history object to pass query params
    
    * remove unused code
    
    * use env variable in script tag and add comment
    
    * implemented initMap
    
    * remove unnecessary comments
    
    * edit places library scrip tag
    
    * remove async defer in script tag
    
    * Forward ref to inner input in ui/Input
    
    * use GeoCode to extract coords from address
    
    * Add center's lat and lng to query params
    
    * remove unecessary comments
    
    * Add simple SearchEntry
    
    * Add left and right icons to SearchEntry
    
    * Improve styling of SearchEntry
    
    * Add secondary text to search suggestions
    
    * Replace bootstrap icon with box icons
    
    * Fix ellipses issue and add onClick to ListEntry
    
    * add forward refs to searchEntry and listEntry
    
    * Implement useQueryParams
    
    * Add QueryParamProvider
    
    * Specify type in query  params
    
    * Pass in centerCoords to url
    
    * Remove search key in history
    
    * Fix access to placeId and highlighting
    
    * Add search to desktop side pane
    
    * Add object schema query params
    
    * Allow setting bounds via view.bounds prop in Map
    
    * Move bounds monitoring to parent
    
    * Change to React context to fix a lot of problems
    
    * Add map view context
    
    * Add icon to search input
    
    * Add TODO to switch to js-api-loader
    
    Co-authored-by: Vasu Chalasani <sc83@illinois.edu>
    Co-authored-by: Jeffrey Tang <jt12@illinois.edu>
    Co-authored-by: code-arman <armanr123@gmail.com>
    Co-authored-by: code-arman <46976119+code-arman@users.noreply.github.com>

--- src/components/desktop/SidePane.js
+++ src/components/desktop/SidePane.js
@@ -1,3 +1,16 @@
-const SidePane = () => <div>Side pane</div>
+import styled from 'styled-components'
+
+import Search from '../search/Search'
+
+const PaneContainer = styled.div`
+  padding: 20px 10px;
+  padding-right: 0px;
+`
+
+const SidePane = () => (
+  <PaneContainer>
+    <Search />
+  </PaneContainer>
+)
 
 export default SidePane
