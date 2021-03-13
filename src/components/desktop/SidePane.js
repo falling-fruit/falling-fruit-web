f08e62063bb1ca9e6707935b3db79304b0e5a864
laurenho025 <laurenh4@illinois.edu>
2021-03-12 20:51:26 -0600

    Side pane routing to display entry details on desktop

--- src/components/desktop/SidePane.js
+++ src/components/desktop/SidePane.js
@@ -1,3 +1,18 @@
-const SidePane = () => <div>Side pane</div>
+import { Route, Switch } from 'react-router-dom'
+
+import EntryDetails from './EntryDetailsWrapper'
+import SidePaneSwitch from './SidePaneSwitch'
+
+const SidePane = () => (
+  <div>
+    <SidePaneSwitch />
+    <Switch>
+      <Route path="/entry/:id">
+        <EntryDetails />
+      </Route>
+      <Route>Side Pane Details</Route>
+    </Switch>
+  </div>
+)
 
 export default SidePane
