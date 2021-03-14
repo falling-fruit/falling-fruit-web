41f9a04290c549a574af862d20a4262c65f993f8
Jeffrey Tang <jt12@illinois.edu>
2021-03-14 03:40:18 -0500

    Remove config file from react-app-rewired

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const rewireStyledComponents = require('react-app-rewire-styled-components')
-
-/* config-overrides.js */
-module.exports = function override(config, env) {
-  config = rewireStyledComponents(config, env)
-  return config
-}
