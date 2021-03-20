d8c34a458284c33ca65c1b4496576efbe257d333
laurenho025 <laurenh4@illinois.edu>
2021-03-20 18:15:50 -0500

    Fix checkbox persistence for muni and invasive checkboxes

--- src/components/filter/Checkboxes.js
+++ src/components/filter/Checkboxes.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import SearchContext from '../search/SearchContext'
 
 /* TODO: Checkbox styling */
-const Checkboxes = ({ handleCheckboxChange }) => (
-  <form>
-    <label>
-      <input name="municipal" type="checkbox" onChange={handleCheckboxChange} />
-      Municipal Tree Inventories
-    </label>
-    <br />
-    <label>
-      <input name="invasive" type="checkbox" onChange={handleCheckboxChange} />
-      Invasive Species Only
-    </label>
-  </form>
-)
+const Checkboxes = ({ handleCheckboxChange }) => {
+  const { filters } = useContext(SearchContext)
+  const { muni, invasive } = filters
+  return (
+    <form>
+      <label>
+        <input
+          checked={muni}
+          name="municipal"
+          type="checkbox"
+          onChange={handleCheckboxChange}
+        />
+        Municipal Tree Inventories
+      </label>
+      <br />
+      <label>
+        <input
+          checked={invasive}
+          name="invasive"
+          type="checkbox"
+          onChange={handleCheckboxChange}
+        />
+        Invasive Species Only
+      </label>
+    </form>
+  )
+}
 
 export default Checkboxes
