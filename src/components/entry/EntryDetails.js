eda344dcec1934a83f9a055066f45b5648eaac1d
Siraj <sirajchokshi@gmail.com>
2021-03-19 21:29:43 -0500

    add new image grid

--- src/components/entry/EntryDetails.js
+++ src/components/entry/EntryDetails.js
@@ -1,4 +1,4 @@
-import { Flag, Map, Star } from '@styled-icons/boxicons-solid'
+import { Flag, ImageAdd,Map, Star } from '@styled-icons/boxicons-solid'
 import React, { useEffect, useState } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import styled from 'styled-components'
@@ -30,10 +30,6 @@ const formatISOString = (dateString) =>
 // Wraps the entire page and gives it a top margin if on mobile
 const EntryDetailsPageContainer = styled.div`
   margin-top: ${(props) => (props.isDesktop ? '0px' : '90px')};
-`
-
-const ImageContainer = styled.img`
-  width: 100%;
 `
 
 // Wraps all text in the container
@@ -104,6 +100,103 @@ const IndividualResourceContainer = styled.small`
   column-gap: 12px;
 `
 
+const PhotoGrid = styled.figure`
+  padding: 0;
+  margin: 0 auto;
+  height: 184px;
+  width: calc(100% - 20px);
+
+  display: grid;
+  grid-template-columns: 1fr 92px;
+  grid-template-rows: 50% 50%;
+  gap: 6.5px;
+  grid-template-areas:
+    'main-image extra-image'
+    'main-image add-more';
+
+  .main-image {
+    grid-area: main-image;
+  }
+  .extra-images {
+    grid-area: extra-image;
+  }
+  .add-more {
+    grid-area: add-more;
+  }
+`
+
+const ImageWrapper = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 4px;
+`
+
+const ExtraImagesWrapper = styled.button`
+  display: block;
+  cursor: pointer;
+  padding: 0;
+  border: none;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+
+  &:disabled {
+    pointer-events: none;
+  }
+
+  & > img {
+    left: 0;
+    top: 0;
+  }
+
+  & > .other-photos-mask {
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    position: absolute;
+    color: white;
+    background: linear-gradient(180deg, rgba(48, 45, 44, 0.4) 0%, #302d2c 100%);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    font-weight: 700;
+    font-size: 13px;
+
+    & > span {
+      font-size: 24px;
+    }
+  }
+`
+
+const ImageUpload = styled.label`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  font-weight: 13px;
+  cursor: pointer;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 2px solid ${({ theme }) => theme.text};
+  font-weight: 700;
+
+  svg {
+    width: 31px;
+    height: auto;
+  }
+
+  input {
+    display: none;
+  }
+`
+
 const EntryDetails = ({ isDesktop }) => {
   const {
     params: { id },
@@ -128,6 +221,11 @@ const EntryDetails = ({ isDesktop }) => {
     console.log('Map Button Clicked')
   }
 
+  const handleViewLightbox = () => {
+    // TODO: connect to lightbox once implemented
+    console.log('Open Image Slideshow/Lightbox')
+  }
+
   const resources = RESOURCES.map(
     ({ title, urlFormatter = (url) => url, urlKey, icon, iconAlt }) =>
       locationTypeData?.[urlKey] && (
@@ -148,10 +246,36 @@ const EntryDetails = ({ isDesktop }) => {
     <EntryDetailsPageContainer isDesktop={isDesktop}>
       {locationData.photos.length > 0 && (
         // TODO: use alt based off of photo description or filename
-        <ImageContainer
-          src={locationData.photos[0].photo.original}
-          alt="entry-details-photo"
-        />
+        <PhotoGrid>
+          <ImageWrapper
+            className="main-image"
+            src={locationData.photos[0].photo.original}
+            alt={locationData.type_names.join(', ')}
+          />
+          {locationData.photos.length > 1 && (
+            <ExtraImagesWrapper
+              onClick={handleViewLightbox}
+              disabled={locationData.photos.length < 3}
+            >
+              {locationData.photos.length > 2 && (
+                <div className="other-photos-mask">
+                  <span>{locationData.photos.length - 2}</span>
+                  Photos
+                </div>
+              )}
+              <ImageWrapper
+                className="extra-images"
+                src={locationData.photos[1].photo.original}
+                alt={locationData.type_names.join(', ')}
+              />
+            </ExtraImagesWrapper>
+          )}
+          <ImageUpload>
+            <ImageAdd />
+            Add Photo
+            <input type="file" />
+          </ImageUpload>
+        </PhotoGrid>
       )}
 
       <EntryDetailsContent>
