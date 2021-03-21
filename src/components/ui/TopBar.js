e63d617d0ab542a0af1b1820802104508c1c2e28
Siraj <sirajchokshi@gmail.com>
2021-03-21 11:08:45 -0500

    add fix drop shadow on top bar

--- src/components/ui/TopBar.js
+++ src/components/ui/TopBar.js
@@ -5,7 +5,6 @@ const TopBar = styled.div`
   left: 0;
   right: 0;
   top: 0;
-  z-index: 1;
   background: ${({ theme }) => theme.background};
   filter: drop-shadow(0px -1px 8px ${({ theme }) => theme.shadow});
   padding: 16px;
