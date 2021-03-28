eb91f3a55371e76e7ea4509506b5ba2b0cedc9e5
laurenho025 <laurenh4@illinois.edu>
2021-03-28 00:50:04 -0500

    Fix custom checkbox styling

--- src/components/filter/Checkboxes.js
+++ src/components/filter/Checkboxes.js
@@ -3,29 +3,29 @@ import React, { useContext } from 'react'
 import SearchContext from '../search/SearchContext'
 import Checkbox from '../ui/Checkbox'
 
-/* TODO: Checkbox styling */
 const Checkboxes = ({ handleCheckboxChange }) => {
   const { filters } = useContext(SearchContext)
   const { muni, invasive } = filters
   return (
     <form>
-      <label htmlFor="muni">
+      <label htmlFor="muni" style={{ display: 'flex', alignItems: 'center' }}>
         <Checkbox
           id="muni"
           checked={muni}
           name="municipal"
-          type="checkbox"
           onChange={handleCheckboxChange}
         />
         Municipal Tree Inventories
       </label>
       <br />
-      <label htmlFor="invasive">
+      <label
+        htmlFor="invasive"
+        style={{ display: 'flex', alignItems: 'center' }}
+      >
         <Checkbox
           id="invasive"
           checked={invasive}
           name="invasive"
-          type="checkbox"
           onChange={handleCheckboxChange}
         />
         Invasive Species Only
--- src/components/ui/Checkbox.js
+++ src/components/ui/Checkbox.js
@@ -4,58 +4,24 @@ import { CustomCheckboxContainer, CustomCheckboxInput } from '@reach/checkbox'
 import { Check } from '@styled-icons/boxicons-regular'
 import React from 'react'
 import styled from 'styled-components/macro'
-//TODO: change all checkboxed to styled components
-const Checkbox = React.forwardRef(
-  ({ className, checked, onChange, ...props }, ref) => (
-    <span className={className}>
-      <CustomCheckboxContainer onChange={onChange}>
-        <CustomCheckboxInput checked={checked} ref={ref} {...props} />
-        {checked === true ? <StyledCheck size="21" /> : <span aria-hidden />}
-      </CustomCheckboxContainer>
-    </span>
-  ),
-)
-Checkbox.displayName = 'Checkbox'
 
 const StyledCheck = styled(Check)`
+  display: ${({ checked }) => (checked ? 'block' : 'none')};
   color: white;
-  position: absolute;
-  top: -3px;
-  left: -3px;
 `
 
-const StyledCheckbox = styled(Checkbox)`
-  [data-reach-custom-checkbox-container] {
-    position: relative;
-    width: 21px;
-    height: 21px;
-    background: ${(props) => {
-      if (props.checked === true) {
-        return ({ theme }) => theme.orange
-      } else {
-        return ({ theme }) => theme.transparentOrange
-      }
-    }};
-    border: 3px solid ${({ theme }) => theme.orange};
-    box-sizing: border-box;
-    border-radius: 4px;
-  }
-
-  [data-reach-custom-checkbox-container] > span {
-    display: block;
-    position: absolute;
-    top: 20%;
-    left: 20%;
-    width: 60%;
-    height: 60%;
-    transition: transform 200ms ease-out, background 200ms ease-out;
-    z-index: 1;
-    background: ${(props) => {
-      if (props.checked === 'mixed') {
-        return ({ theme }) => theme.orange
-      }
-    }};
-  }
+const StyledCustomCheckboxContainer = styled(CustomCheckboxContainer)`
+  border: 3px solid ${({ theme }) => theme.orange};
+  border-radius: 4px;
+  background: ${({ checked, theme }) =>
+    checked ? theme.orange : theme.transparentOrange};
 `
 
-export default StyledCheckbox
+const Checkbox = ({ checked, name, onChange, id }) => (
+  <StyledCustomCheckboxContainer checked={checked} onChange={onChange}>
+    <CustomCheckboxInput id={id} name={name} />
+    <StyledCheck checked={checked} />
+  </StyledCustomCheckboxContainer>
+)
+
+export default Checkbox
