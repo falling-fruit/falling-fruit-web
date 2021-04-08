da3407f314569599256749d3c191704a65144545
Archna-1 <archnas2@illinois.edu>
2021-04-07 20:41:59 -0500

    Move margins to icons and make them conditional

--- src/components/ui/ListEntry.js
+++ src/components/ui/ListEntry.js
@@ -10,6 +10,8 @@ const Icons = styled.div`
   & > *:not(:last-child) {
     margin: 0 8px;
   }
+  margin-right: ${(props) => (props.prepend ? '18px' : 'none')};
+  margin-left: ${(props) => (!props.prepend ? '18px' : 'none')};
 `
 const PrimaryText = styled.div`
   font-weight: bold;
@@ -34,7 +36,6 @@ const TextContainer = styled.div`
   flex-direction: column;
   color: ${({ theme }) => theme.secondaryText};
   justify-content: center;
-  margin-left: 18px;
   flex: 1;
   overflow: hidden;
   white-space: nowrap;
@@ -50,12 +51,12 @@ const TextContainer = styled.div`
 const ListEntry = React.forwardRef(
   ({ leftIcons, primaryText, secondaryText, rightIcons, ...props }, ref) => (
     <ListContainer ref={ref} {...props}>
-      <Icons>{leftIcons}</Icons>
+      {leftIcons && <Icons prepend>{leftIcons}</Icons>}
       <TextContainer>
         <PrimaryText className="primaryText">{primaryText}</PrimaryText>
         <SecondaryText className="secondaryText">{secondaryText}</SecondaryText>
       </TextContainer>
-      <Icons>{rightIcons}</Icons>
+      {rightIcons && <Icons append>{rightIcons}</Icons>}
     </ListContainer>
   ),
 )
@@ -64,7 +65,7 @@ ListEntry.displayName = 'ListEntry'
 
 ListEntry.propTypes = {
   leftIcons: PropTypes.node,
-  primaryText: PropTypes.string,
+  primaryText: PropTypes.node,
   secondaryText: PropTypes.string,
   rightIcons: PropTypes.node,
 }
