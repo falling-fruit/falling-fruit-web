3f68e8c1884de9d4fd657245c7eb877a32738ab2
Vasu Chalasani <sc83@illinois.edu>
2021-04-08 21:33:41 -0500

    Add modal styling

--- src/components/mobile/SettingsPage.js
+++ src/components/mobile/SettingsPage.js
@@ -1,9 +1,11 @@
 import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular'
 import { Cog, Flag, Star } from '@styled-icons/boxicons-solid'
+import { useState } from 'react'
 
 import Button from '../ui/Button'
 import Input from '../ui/Input'
 import ListEntry from '../ui/ListEntry'
+import Modal from '../ui/Modal'
 import {
   AccordionItem,
   AccordionPanel,
@@ -13,63 +15,77 @@ import {
 import { Slider } from '../ui/Slider'
 import { Tag, TagList } from '../ui/Tag'
 
-const SettingsPage = () => (
-  <>
-    <p>Settings</p>
-    <Button icon={<Star />}>Review</Button>
-    <Button icon={<Flag />} secondary>
-      Report
-    </Button>
-    <br />
-    <br />
-    <Input
-      placeholder="Search for a location..."
-      onChange={(e) => console.log(e.target.value)}
-      onEnter={(e) => window.alert(`Received:\n${e?.target?.value}`)}
-      icon={<Search />}
-    />
-    <br />
-    <Input
-      label="Demo Form Input"
-      placeholder="'Sugar Maple'"
-      onChange={(e) => console.log(e.target.value)}
-    />
-    <br />
-    <TagList>
-      {['Rebecca', 'Jeffrey'].map((name) => (
-        <Tag key={name}>{name}</Tag>
-      ))}
-    </TagList>
-    <br />
-    <ListEntry
-      leftIcons={<Star size="16" />}
-      primaryText="Entry"
-      secondaryText="0.3 miles"
-    />
-    <ListEntry
-      primaryText="Entry"
-      secondaryText="0.3 miles"
-      rightIcons={[<Star size="16" key={1} />, <Star size="16" key={2} />]}
-      size={57}
-    />
-    <ListEntry primaryText="Entry" rightIcons={<Star size="16" />} />
-    <SettingsAccordion>
-      <AccordionItem>
-        <SettingsAccordionButton LeftIcon={Cog} text="Options" />
-        <AccordionPanel>
-          Here are some detailed instructions about doing a thing. I am very
-          complex and probably contain a lot of content, so a user can hide or
-          show me by clicking the button above.
-        </AccordionPanel>
-      </AccordionItem>
-    </SettingsAccordion>
+const SettingsPage = () => {
+  const [showDialog, setShowDialog] = useState(false)
+  const open = () => setShowDialog(true)
+  const close = () => setShowDialog(false)
+  return (
+    <>
+      <p>Settings</p>
+      <Button icon={<Star />}>Review</Button>
+      <Button icon={<Flag />} secondary>
+        Report
+      </Button>
+      <br />
+      <br />
+      <Input
+        placeholder="Search for a location..."
+        onChange={(e) => console.log(e.target.value)}
+        onEnter={(e) => window.alert(`Received:\n${e?.target?.value}`)}
+        icon={<Search />}
+      />
+      <br />
+      <Input
+        label="Demo Form Input"
+        placeholder="'Sugar Maple'"
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <br />
+      <TagList>
+        {['Rebecca', 'Jeffrey'].map((name) => (
+          <Tag key={name}>{name}</Tag>
+        ))}
+      </TagList>
+      <br />
+      <ListEntry
+        leftIcons={<Star size="16" />}
+        primaryText="Entry"
+        secondaryText="0.3 miles"
+      />
+      <ListEntry
+        primaryText="Entry"
+        secondaryText="0.3 miles"
+        rightIcons={[<Star size="16" key={1} />, <Star size="16" key={2} />]}
+        size={57}
+      />
+      <ListEntry primaryText="Entry" rightIcons={<Star size="16" />} />
+      <SettingsAccordion>
+        <AccordionItem>
+          <SettingsAccordionButton LeftIcon={Cog} text="Options" />
+          <AccordionPanel>
+            Here are some detailed instructions about doing a thing. I am very
+            complex and probably contain a lot of content, so a user can hide or
+            show me by clicking the button above.
+          </AccordionPanel>
+        </AccordionItem>
+      </SettingsAccordion>
 
-    <Slider
-      style={{ margin: '0 40px' }}
-      labels={['Label 1', null, 'Label 3', 'Label 4', 'Label 5']}
-      steps={5}
-    />
-  </>
-)
+      <Slider
+        style={{ margin: '0 40px' }}
+        labels={['Label 1', null, 'Label 3', 'Label 4', 'Label 5']}
+        steps={5}
+      />
+
+      <br />
+      <br />
+      <button onClick={open}>Show Dialog</button>
+      <Modal isOpen={showDialog} onDismiss={close}>
+        <p>Pass the button ref to Dialog and the button.</p>
+        <button onClick={close}>Not me</button>
+        <button onClick={close}>Got me!</button>
+      </Modal>
+    </>
+  )
+}
 
 export default SettingsPage
---
+++ src/components/ui/Modal.js
@@ -0,0 +1,13 @@
+import '@reach/dialog/styles.css'
+
+import { Dialog } from '@reach/dialog'
+import styled from 'styled-components/macro'
+
+const StyledDialog = styled(Dialog)`
+  border-radius: 23px;
+  width: 75%;
+  height: 70%;
+`
+const Modal = ({ ...props }) => <StyledDialog {...props}> </StyledDialog>
+
+export default Modal
