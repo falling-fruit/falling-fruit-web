8bdf7953aaef4bfb2500ca1dcddefd87fcb1d40c
Vasudha Chalasani <40867028+vasuchal@users.noreply.github.com>
2021-04-08 22:09:04 -0500

    🧑🏼‍🦽 Form progress bar (#69)
    * Create components for progress bar
    
    * Finish UI of progress bar
    
    * Clean progress bar step styling
    
    * Add logic handling
    
    * add event handlers
    
    * Add checkmark and onchange implementation
    
    * Add final changes
    
    * Delete primaryText and edit styling
    
    * Override background color in StyledCircleIcon
    
    * Remove theme import and refactor
    
    * Change stepNumber to children
    
    * Remove unused stepNumber prop
    
    Co-authored-by: Vasu Chalasani <sc83@illinois.edu>

--- src/components/mobile/SettingsPage.js
+++ src/components/mobile/SettingsPage.js
@@ -1,9 +1,11 @@
 import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular'
 import { Cog, Flag, Star } from '@styled-icons/boxicons-solid'
+import { useState } from 'react'
 
 import Button from '../ui/Button'
 import Input from '../ui/Input'
 import ListEntry from '../ui/ListEntry'
+import ProgressBar from '../ui/ProgressBar'
 import {
   AccordionItem,
   AccordionPanel,
@@ -13,63 +15,73 @@ import {
 import { Slider } from '../ui/Slider'
 import { Tag, TagList } from '../ui/Tag'
 
-const SettingsPage = () => (
-  <>
-    <p>Settings</p>
-    <Button icon={<Star />}>Review</Button>
-    <Button icon={<Flag />} secondary>
-      Report
-    </Button>
-    <br />
-    <br />
-    <Input
-      placeholder="Search for a location..."
-      onChange={(e) => console.log(e.target.value)}
-      onEnter={(e) => window.alert(`Received:\n${e?.target?.value}`)}
-      icon={<Search />}
-    />
-    <br />
-    <Input
-      label="Demo Form Input"
-      placeholder="'Sugar Maple'"
-      onChange={(e) => console.log(e.target.value)}
-    />
-    <br />
-    <TagList>
-      {['Rebecca', 'Jeffrey'].map((name) => (
-        <Tag key={name}>{name}</Tag>
-      ))}
-    </TagList>
-    <br />
-    <ListEntry
-      leftIcons={<Star size="16" />}
-      primaryText="Entry"
-      secondaryText="0.3 miles"
-    />
-    <ListEntry
-      primaryText="Entry"
-      secondaryText="0.3 miles"
-      rightIcons={[<Star size="16" key={1} />, <Star size="16" key={2} />]}
-      size={57}
-    />
-    <ListEntry primaryText="Entry" rightIcons={<Star size="16" />} />
-    <SettingsAccordion>
-      <AccordionItem>
-        <SettingsAccordionButton LeftIcon={Cog} text="Options" />
-        <AccordionPanel>
-          Here are some detailed instructions about doing a thing. I am very
-          complex and probably contain a lot of content, so a user can hide or
-          show me by clicking the button above.
-        </AccordionPanel>
-      </AccordionItem>
-    </SettingsAccordion>
+const SettingsPage = () => {
+  const [currentStep, setCurrentStep] = useState(2)
+  return (
+    <>
+      <p>Settings</p>
+      <Button icon={<Star />}>Review</Button>
+      <Button icon={<Flag />} secondary>
+        Report
+      </Button>
+      <br />
+      <br />
+      <Input
+        placeholder="Search for a location..."
+        onChange={(e) => console.log(e.target.value)}
+        onEnter={(e) => window.alert(`Received:\n${e?.target?.value}`)}
+        icon={<Search />}
+      />
+      <br />
+      <Input
+        label="Demo Form Input"
+        placeholder="'Sugar Maple'"
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <br />
+      <TagList>
+        {['Rebecca', 'Jeffrey'].map((name) => (
+          <Tag key={name}>{name}</Tag>
+        ))}
+      </TagList>
+      <br />
+      <ListEntry
+        leftIcons={<Star size="16" />}
+        primaryText="Entry"
+        secondaryText="0.3 miles"
+      />
+      <ListEntry
+        primaryText="Entry"
+        secondaryText="0.3 miles"
+        rightIcons={[<Star size="16" key={1} />, <Star size="16" key={2} />]}
+        size={57}
+      />
 
-    <Slider
-      style={{ margin: '0 40px' }}
-      labels={['Label 1', null, 'Label 3', 'Label 4', 'Label 5']}
-      steps={5}
-    />
-  </>
-)
+      <ListEntry primaryText="Entry" rightIcons={<Star size="16" />} />
+      <SettingsAccordion>
+        <AccordionItem>
+          <SettingsAccordionButton LeftIcon={Cog} text="Options" />
+          <AccordionPanel>
+            Here are some detailed instructions about doing a thing. I am very
+            complex and probably contain a lot of content, so a user can hide or
+            show me by clicking the button above.
+          </AccordionPanel>
+        </AccordionItem>
+      </SettingsAccordion>
+      <Slider
+        style={{ margin: '0 40px' }}
+        labels={['Label 1', null, 'Label 3', 'Label 4', 'Label 5']}
+        steps={5}
+      />
+      <br />
+      <br />
+      <ProgressBar
+        labels={['step1', 'step2', 'step3', 'step4', 'step5']}
+        step={currentStep}
+        onChange={setCurrentStep}
+      />
+    </>
+  )
+}
 
 export default SettingsPage
---
+++ src/components/ui/ProgressBarStep.js
@@ -0,0 +1,62 @@
+import { Check } from '@styled-icons/boxicons-regular'
+import styled from 'styled-components/macro'
+
+import CircleIcon from '../ui/CircleIcon'
+
+const StyledCircleIcon = styled(CircleIcon)`
+  color: ${({ $status, theme }) =>
+    $status === 'incomplete' ? theme.secondaryText : theme.orange};
+  background-color: ${({ $status, theme }) =>
+    $status === 'incomplete'
+      ? theme.secondaryBackground
+      : theme.transparentOrange};
+  border: ${({ $status, theme }) =>
+      $status === 'incomplete' ? theme.secondaryText : theme.orange}
+    2px solid;
+  svg {
+    color: ${({ theme }) => theme.orange};
+  }
+`
+
+const StyledProgressBarStep = styled.div`
+  position: relative;
+  font-size: 15px;
+  color: ${({ $status, theme }) =>
+    $status === 'incomplete' ? theme.tertiaryText : theme.orange};
+
+  & + & {
+    flex-grow: 1;
+    display: flex;
+    align-items: flex-end;
+    flex-direction: column;
+  }
+
+  & + &::before {
+    top: calc(50% - 0.6rem);
+    right: 36px;
+    height: 100%;
+    position: absolute;
+    align-self: center;
+    content: '';
+    border-top: ${({ theme }) => theme.secondaryText} 2.5px solid;
+    width: calc(100% - 34px);
+  }
+
+  // TODO: fix specificity of ::before CSS
+
+  &::before {
+    border-color: ${({ $status, theme }) =>
+      $status === 'incomplete' ? theme.secondaryText : theme.orange} !important;
+  }
+`
+
+const ProgressBarStep = ({ label, children, status, onClick }) => (
+  <StyledProgressBarStep $status={status}>
+    <StyledCircleIcon $status={status} onClick={onClick}>
+      {status === 'complete' ? <Check /> : <div>{children}</div>}
+    </StyledCircleIcon>
+    <div>{label}</div>
+  </StyledProgressBarStep>
+)
+
+export default ProgressBarStep
