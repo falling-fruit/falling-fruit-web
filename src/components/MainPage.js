52e67e0c3e51120e11aff6822bef5c379f0fc516
Vasu Chalasani <sc83@illinois.edu>
2021-04-21 13:29:25 -0500

    Add Search Provider

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,31 +2,28 @@ import { useState } from 'react'
 
 import MapContext from '../contexts/MapContext'
 import MapProvider from '../contexts/MapProvider'
-import SearchContext, { DEFAULT_FILTERS } from '../contexts/SearchContext'
+import SearchContext from '../contexts/SearchContext'
+import SearchProvider from '../contexts/SearchProvider'
 import SettingsContext, { DEFAULT_SETTINGS } from '../contexts/SettingsContext'
 import { useIsDesktop } from '../utils/useBreakpoint'
 import DesktopLayout from './desktop/DesktopLayout'
 import MobileLayout from './mobile/MobileLayout'
 
 const MainPage = () => {
-  // const [view, setView] = useState(DEFAULT_VIEW_STATE)
-  const [viewport, setViewport] = useState(null)
-  const [filters, setFilters] = useState(DEFAULT_FILTERS)
   const [showLabels, setShowLabels] = useState(DEFAULT_SETTINGS.showLabels)
 
   const isDesktop = useIsDesktop()
   const layout = isDesktop ? <DesktopLayout /> : <MobileLayout />
   const MapContextProvider = MapProvider(MapContext)
+  const SearchContextProvider = SearchProvider(SearchContext)
 
   return (
     <MapContextProvider>
-      <SearchContext.Provider
-        value={{ viewport, setViewport, filters, setFilters }}
-      >
+      <SearchContextProvider>
         <SettingsContext.Provider value={{ showLabels, setShowLabels }}>
           {layout}
         </SettingsContext.Provider>
-      </SearchContext.Provider>
+      </SearchContextProvider>
     </MapContextProvider>
   )
 }
--- src/contexts/MapProvider.js
+++ src/contexts/MapProvider.js
@@ -2,13 +2,16 @@ import { useState } from 'react'
 
 import { DEFAULT_VIEW_STATE } from '../contexts/MapContext'
 
-const MapProvider = (MapContext, children) => {
-  const [view, setView] = useState(DEFAULT_VIEW_STATE)
-  return (
-    <MapContext.Provider value={{ view, setView }}>
-      {children}
-    </MapContext.Provider>
-  )
+const MapProvider = (MapContext) => {
+  const MapContextProvider = ({ children, ...props }) => {
+    const [view, setView] = useState(DEFAULT_VIEW_STATE)
+    return (
+      <MapContext.Provider value={{ view, setView }} {...props}>
+        {children}
+      </MapContext.Provider>
+    )
+  }
+  return MapContextProvider
 }
 
 export default MapProvider
