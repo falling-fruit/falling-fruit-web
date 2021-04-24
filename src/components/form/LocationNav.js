4d98d3742d25e03a1db6107790d3f8798404bec4
Jeffrey Tang <jt12@illinois.edu>
2021-04-24 12:41:48 -0500

    Remove "confirm location"

--- src/components/form/LocationNav.js
+++ src/components/form/LocationNav.js
@@ -1,10 +1,7 @@
 import { Check, X } from '@styled-icons/boxicons-regular'
-import { useContext, useState } from 'react'
 import { Route, Switch, useHistory } from 'react-router-dom'
-import { useUpdateEffect } from 'react-use'
 import styled from 'styled-components'
 
-import MapContext from '../map/MapContext'
 import { theme } from '../ui/GlobalStyle'
 import IconButton from '../ui/IconButton'
 import TopBarNav from '../ui/TopBarNav'
@@ -15,25 +12,13 @@ const Instructions = styled.span`
 
 const LocationNav = () => {
   const history = useHistory()
-  const {
-    view: { center },
-  } = useContext(MapContext)
-  const [isConfirming, setIsConfirming] = useState(false)
-
-  useUpdateEffect(() => {
-    setIsConfirming(true)
-  }, [center.lat, center.lng])
 
   return (
     <Switch>
       <Route path="/entry/new" exact>
         <TopBarNav
           left={
-            <Instructions>
-              {isConfirming
-                ? 'Confirm Location'
-                : 'Choose a location for your new entry.'}
-            </Instructions>
+            <Instructions>Choose a location for your new entry.</Instructions>
           }
           rightIcons={
             <>
@@ -43,23 +28,19 @@ const LocationNav = () => {
                 raised
                 size={54}
                 onClick={() => {
-                  if (isConfirming) {
-                    setIsConfirming(false)
-                  } else {
-                    history.push('/map')
-                  }
+                  history.push('/map')
                 }}
               />
-              {isConfirming && (
-                <IconButton
-                  label="Confirm choose location"
-                  icon={<Check />}
-                  raised
-                  size={54}
-                  color={theme.green}
-                  onClick={() => history.push('/entry/new/details')}
-                />
-              )}
+              <IconButton
+                label="Confirm choose location"
+                icon={<Check />}
+                raised
+                size={54}
+                color={theme.green}
+                onClick={() => {
+                  history.push('/entry/new/details')
+                }}
+              />
             </>
           }
         />
