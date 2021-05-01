650e24769915c9df1d8a7bfb7a976f5c21d40559
Vasu Chalasani <sc83@illinois.edu>
2021-05-01 12:29:36 -0500

    Add language detection and afallback

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,26 @@
 import i18n from 'i18next'
-// import LanguageDetector from 'i18next-browser-languagedetector'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
-
-// the translations
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(Backend)
+  .use(LanguageDetector)
   .init({
+    react: {
+      useSuspense: false,
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
     backend: {
       // for all available options read the backend's repository readme file
       loadPath: '/locales/{{lng}}.json',
     },
   })
 
-//how to use language detector and namespaces for this (add init)
+//how to use language detector and namespaces for this (add init), fallback UI, (doesn't work without lng)
+
 export default i18n
