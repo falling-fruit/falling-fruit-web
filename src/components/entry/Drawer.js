f5b32ad9c55bfaf1044bc4264012a2c34f3d7778
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-05-02 11:28:09 -0500

    add migrate to cupertino pane for drawer

--- src/components/entry/Drawer.js
+++ src/components/entry/Drawer.js
@@ -1,18 +1,7 @@
-import React, {
-  forwardRef,
-  useEffect,
-  useImperativeHandle,
-  useRef,
-  useState,
-} from 'react'
-import { animated, useSpring } from 'react-spring'
-import { useDrag } from 'react-use-gesture'
+import { CupertinoPane } from 'cupertino-pane'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components/macro'
-
-// TODO Use Reach Hook for Window Height
-const windowHeight = window.innerHeight
-
-const OPEN_NAV_SIZE = 0.75
 
 const MainContainer = styled.div`
   position: absolute;
@@ -23,221 +12,33 @@ const MainContainer = styled.div`
   left: 0;
 `
 
-const Overflow = styled.div`
-  position: absolute;
-  background-color: white;
-  width: 100%;
-  bottom: 0px;
-  top: 0;
-  ${({ open }) =>
-    open ? 'overflow: hidden; border-radius: 44px 44px 0 0' : ''};
+function BottomSheet({ id, children }) {
+  const history = useHistory()
 
-  & > button#toggle {
-    margin: 0;
-    width: 100%;
-    background: none;
-    border: none;
-    /* height: 3em; */
-
-    &::before {
-      content: '';
-      height: 6px;
-      width: 4em;
-      position: absolute;
-      top: ${({ open }) => (open ? '1em' : '-1em')};
-      left: calc(50% - 2em);
-      background: ${({ theme }) => theme.secondaryText};
-      border-radius: 3px;
-    }
-    &::after {
-      content: '';
-      height: 6em;
-      width: 100%;
-      position: absolute;
-      top: -3em;
-      cursor: pointer;
-    }
-  }
-
-  *:not(#toggle) {
-    pointer-events: ${({ open }) => (open ? 'auto' : 'none !important')};
-  }
-`
-
-const EntryWrapper = styled.div`
-  height: 100%;
-  overflow: auto;
-`
-
-const Container = styled(animated.div)`
-  position: absolute;
-  z-index: 999;
-  width: 100%;
-  max-height: ${OPEN_NAV_SIZE * windowHeight}px;
-  height: 100%;
-
-  &:after {
-    content: '';
-    position: absolute;
-    bottom: -${windowHeight * 0.04}px;
-    height: ${windowHeight * 0.04}px;
-    width: 100%;
-  }
-`
-
-// data-pull should be set on element that can be pulled
-const canPull = (element) => {
-  if (element && element.dataset && element.dataset.pull) {
-    return true
-  } else if (element.parentNode) {
-    return canPull(element.parentNode)
-  }
-
-  return false
-}
-
-const PullContainer = forwardRef(
-  ({ children, overflowHeight, className }, ref) => {
-    const trans = (y) => `translateY(${y}px)`
-    const topInterpolate = (px) => `calc(100% - ${px}px)`
-    const [open, setOpen] = useState(false)
-    const refContainer = useRef()
-
-    const [styleProps, set] = useSpring(() => ({
-      y: 0,
-      top: overflowHeight,
-      config: {
-        mass: 1,
-        tension: 350,
-        friction: 40,
+  useEffect(() => {
+    const drawer = new CupertinoPane(`#${id}`, {
+      breaks: {
+        top: { enabled: true, height: 500, bounce: true },
+        middle: { enabled: true, height: 325 },
       },
-    }))
-
-    const setContainerOpen = (shouldOpen) => {
-      if (shouldOpen === open) {
-        return
-      }
-
-      if (shouldOpen) {
-        set({
-          top: 0,
-          y: -OPEN_NAV_SIZE * windowHeight,
-        })
-      } else {
-        set({
-          top: overflowHeight,
-          y: 0,
-        })
-      }
-      setOpen(shouldOpen)
-    }
-
-    useImperativeHandle(ref, () => ({
-      setOpen(shouldOpen) {
-        setContainerOpen(shouldOpen)
-      },
-    }))
-
-    useEffect(() => {
-      set({ top: overflowHeight })
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [overflowHeight])
-
-    const bind = useDrag(({ down, movement, cancel, event }) => {
-      if (event.persist) {
-        event.persist()
-        if (event.target && !canPull(event.target)) {
-          cancel()
-        }
-      }
-
-      const { top } = refContainer.current.getBoundingClientRect()
-      let setTop = overflowHeight
-      let setY = movement[1]
-      const percentOverflow = (overflowHeight * 100) / windowHeight
-      const percentOpened = ((windowHeight - top) / windowHeight) * 100
-
-      // If not down -> if release pull
-      if (!down) {
-        // If already open and percent openend is less than the opened percentage + 10
-        // Else let open
-        if (open) {
-          if (percentOpened < OPEN_NAV_SIZE * 100 - 10) {
-            setY = 0
-            setOpen(false)
-          } else {
-            setY = -OPEN_NAV_SIZE * windowHeight
-            setTop = 0
-          }
-          // If not open and open more than 5% of overflow then open it
-          // Else stay closed
-        } else if (percentOpened > percentOverflow + 5) {
-          setY = -OPEN_NAV_SIZE * windowHeight
-          setTop = 0
-          setOpen(true)
-        } else {
-          setY = 0
-        }
-      } else {
-        // If is pulling and pull to bottom or top more than 2% then cancel
-        // If is pulling and open, set top to opened value
-        if (open) {
-          setTop = OPEN_NAV_SIZE * windowHeight
-        }
-        if (percentOpened < 0 || percentOpened > OPEN_NAV_SIZE * 100 + 2) {
-          cancel()
-        }
-      }
-      set({
-        y: setY,
-        top: setTop,
-      })
+      initialBreak: 'middle',
+      pushMinHeight: 370,
+      fastSwipeClose: true,
+      bottomClose: true,
+      buttonDestroy: false,
+      onDidDismiss: () => history.push('/map'),
     })
 
-    return (
-      <Container
-        className={className}
-        ref={refContainer}
-        {...bind()}
-        style={{
-          transform: styleProps.y.interpolate(trans),
-          top: styleProps.top.interpolate(topInterpolate),
-        }}
-      >
-        {
-          // TODO: add back `data-pull` attr for gestures
-        }
-        <Overflow open={open}>
-          <button
-            id="toggle"
-            data-_pull
-            onClick={() => setContainerOpen(!open)}
-          />
-          <EntryWrapper
-            onClick={() => !open && setContainerOpen(true)}
-            role={!open && 'button'}
-            tabIndex={-1}
-            onKeyDown={() => !open && console.log('entered')}
-          >
-            {children}
-          </EntryWrapper>
-        </Overflow>
-      </Container>
-    )
-  },
-)
+    setTimeout(() => drawer.present({ animate: true }), 0)
+  }, [id])
 
-PullContainer.defaultProps = {
-  overflowHeight: 0,
-  children: null,
+  return <div id={id}>{children}</div>
 }
-
-PullContainer.displayName = 'PullContainer'
 
 export default function Drawer({ children }) {
   return (
     <MainContainer>
-      <PullContainer overflowHeight={275}>{children}</PullContainer>
+      <BottomSheet id={'map-bottom-drawer'}>{children}</BottomSheet>
     </MainContainer>
   )
 }
