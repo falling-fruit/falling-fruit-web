472ae5cd1951229efc65dd9606b97ec4c13a9935
Jeffrey Tang <jt12@illinois.edu>
2021-05-26 01:23:31 -0500

    Add connected-react-router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import WindowSize from '@reach/window-size'
+import { ConnectedRouter } from 'connected-react-router'
 import { Provider } from 'react-redux'
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import ComponentDemos from './components/ComponentDemos'
 import MainPage from './components/MainPage'
 import GlobalStyle, { theme } from './components/ui/GlobalStyle'
-import store from './redux/store'
+import store, { history } from './redux/store'
 
 const App = () => (
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <Router>
+    <ConnectedRouter history={history}>
+      <ThemeProvider theme={theme}>
         <Switch>
           <Route exact path="/">
             <Redirect to="/map" />
@@ -28,11 +24,11 @@ const App = () => (
             <MainPage />
           </Route>
         </Switch>
-      </Router>
-      <WindowSize>
-        {(windowSize) => <GlobalStyle windowSize={windowSize} />}
-      </WindowSize>
-    </ThemeProvider>
+        <WindowSize>
+          {(windowSize) => <GlobalStyle windowSize={windowSize} />}
+        </WindowSize>
+      </ThemeProvider>
+    </ConnectedRouter>
   </Provider>
 )
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
 
 import settingsReducer from './settingsSlice'
 
+export const history = createBrowserHistory()
+
 export default configureStore({
-  reducer: { settings: settingsReducer },
+  reducer: { settings: settingsReducer, router: connectRouter(history) },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(routerMiddleware(history)),
 })
