a8b112e787608524544c3897cc85c4f54ce1b53c
Jeffrey Tang <jt12@illinois.edu>
2021-05-29 03:18:08 -0500

    Don't show accordion if no URLs

--- src/components/entry/TypesHeader.js
+++ src/components/entry/TypesHeader.js
@@ -5,24 +5,41 @@ import {
   TypesAccordionItem,
   TypesAccordionPanel,
 } from '../ui/TypesAccordion'
+import TypeTitle from '../ui/TypeTitle'
 import ResourceList from './ResourceList'
 
 const TypesHeader = ({ typeIds }) => {
   const { typesById, getCommonName, getScientificName } = useTypesById()
 
+  console.log(typeIds, typesById?.[218])
+
   return (
     <TypesAccordion>
-      {typeIds.map((id) => (
-        <TypesAccordionItem key={id}>
-          <TypesAccordionButton
-            commonName={getCommonName(id)}
-            scientificName={getScientificName(id)}
-          />
-          <TypesAccordionPanel>
-            <ResourceList urls={typesById[id].urls} />
-          </TypesAccordionPanel>
-        </TypesAccordionItem>
-      ))}
+      {typeIds
+        .filter((id) => typesById?.[id])
+        .map((id) => {
+          const typeTitle = (
+            <TypeTitle
+              key={id}
+              primaryText={getCommonName(id)}
+              secondaryText={getScientificName(id)}
+            />
+          )
+
+          if (Object.keys(typesById[id].urls).length > 0) {
+            // At least 1 URL
+            return (
+              <TypesAccordionItem key={id}>
+                <TypesAccordionButton>{typeTitle}</TypesAccordionButton>
+                <TypesAccordionPanel>
+                  <ResourceList urls={typesById[id].urls} />
+                </TypesAccordionPanel>
+              </TypesAccordionItem>
+            )
+          } else {
+            return typeTitle
+          }
+        })}
     </TypesAccordion>
   )
 }
