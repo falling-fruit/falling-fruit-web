1cf67e05efee7c05eb18acd1b41c8e3b79dcefdf
Riya Jain <riyajain@wirelessprv-10-192-167-80.near.illinois.edu>
2021-09-27 19:48:13 -0500

    Update changes of new header

--- src/components/desktop/Header.js
+++ src/components/desktop/Header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components/macro'
 
 const StyledHeader = styled.header`
   height: 70px;
-  background-color: ${({ theme }) => theme.orange};
+  background-color: ${({ theme }) => theme.background};
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -18,6 +18,7 @@ const StyledHeader = styled.header`
 
   nav {
     height: 100%;
+    z-index: 2;
     ul {
       list-style: none;
       padding: 0;
@@ -27,12 +28,56 @@ const StyledHeader = styled.header`
       li {
         display: inline-grid;
         place-items: center;
-        font-weight: bold;
         width: 110px;
         margin: 0;
         height: 100%;
-        color: ${({ theme }) => theme.background};
+        color: ${({ theme }) => theme.text};
         cursor: pointer;
+
+        li,
+        &.active {
+          background-color: ${({ theme }) => theme.secondaryBackground};
+          color: ${({ theme }) => theme.orange};
+          box-sizing: border-box;
+          border-bottom: 3px solid ${({ theme }) => theme.orange};
+        }
+
+        .dropdown {
+          position: relative;
+          display: inline-block;
+        }
+
+        .dropbtn {
+          background-color: ${({ theme }) => theme.background};
+          color: ${({ theme }) => theme.text};
+          padding: 16px;
+          font-size: 16px;
+          border: none;
+        }
+
+        .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: ${({ theme }) => theme.background};
+          min-width: 160px;
+          box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+          z-index: 1;
+        }
+
+        .dropdown-content a {
+          color: black;
+          padding: 12px 16px;
+          text-decoration: none;
+          display: block;
+        }
+
+        .dropdown:hover .dropdown-content {
+          display: block;
+        }
+
+        .dropdown:hover .dropbtn {
+          background-color: ${({ theme }) => theme.secondaryBackground};
+        }
 
         button {
           svg {
@@ -65,11 +110,21 @@ const Header = () => {
   const { t } = useTranslation()
   return (
     <StyledHeader>
-      <img src="/logo_white.svg" alt="Falling Fruit Logo" />
+      <img src="/logo_orange.svg" alt="Falling Fruit Logo" />
       <nav>
         <ul>
           <li className="active">{t('Map')}</li>
-          <li>{t('About')}</li>
+          <li>{t('Page 2')}</li>
+          <li>
+            <div className="dropdown">
+              <button className="dropbtn">Dropdown</button>
+              <div className="dropdown-content">
+                <a href="#hi">Page 1</a>
+                <a href="#hi">Page 2</a>
+                <a href="#hi">Page 3</a>
+              </div>
+            </div>
+          </li>
           <li>
             <button>
               <User /> {t('Login')}
