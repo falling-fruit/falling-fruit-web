4313109f2eb57c7b0167f1d9bf2f70ab61048058
Arman Rafati <armanrafati@wirelessprv-10-192-137-223.near.illinois.edu>
2021-09-30 22:37:35 -0500

    Add styling and link mapping

--- src/components/ui/DataTable.js
+++ src/components/ui/DataTable.js
@@ -1,8 +1,39 @@
+import { Flag } from '@styled-icons/boxicons-solid'
 import DataTable from 'react-data-table-component'
+import styled from 'styled-components/macro'
+
+const DataTableWrapper = styled.div`
+  .rdt {
+    &_TableHeadRow {
+      font-style: normal;
+      font-weight: bold;
+      font-size: 14px;
+    }
+    &_TableCell {
+      font-size: 14px;
+      &[data-column-id='scientific_name'] {
+        font-style: italic;
+      }
+    }
+  }
+`
 
 const DataTableComponent = ({ data, columns, sorted_columns }) => {
   function set_sortable_columns() {
-    for (var i = 0; i < sorted_columns.length; i++) {
+    if (columns.has('links')) {
+      let json = columns.get('links')
+      json.format = (row) => {
+        const links = row.links
+        return links.map((link, index) => (
+          <a key={index} href={link}>
+            <Flag size={20} />
+          </a>
+        ))
+      }
+      columns.set('links', json)
+    }
+
+    for (let i = 0; i < sorted_columns.length; i++) {
       let key = sorted_columns[i].id
       if (!columns.has(key)) {
         continue
@@ -18,7 +49,11 @@ const DataTableComponent = ({ data, columns, sorted_columns }) => {
     return Array.from(columns.values())
   }
 
-  return <DataTable pagination columns={set_sortable_columns()} data={data} />
+  return (
+    <DataTableWrapper>
+      <DataTable pagination columns={set_sortable_columns()} data={data} />
+    </DataTableWrapper>
+  )
 }
 
 export default DataTableComponent
