6de16d5b22695ab8da1e4b7de5ac4b7c75558211
laurenho025 <laurenh4@illinois.edu>
2021-10-06 20:04:00 -0500

    Fix tree search by filtering on node title

--- src/components/filter/RCTreeSelect.js
+++ src/components/filter/RCTreeSelect.js
@@ -2,24 +2,36 @@ import './rc-tree.css'
 import './rc-select.css'
 
 import TreeSelect from 'rc-tree-select'
+import styled from 'styled-components/macro'
+
+const TreeSelectContainer = styled.div`
+  .rc-tree-select {
+    width: 100%;
+  }
+  .rc-tree-select-selector {
+    border-radius: 23px;
+    border: 1px solid ${({ theme }) => theme.secondaryBackground};
+  }
+`
 
 const RCTreeSelect = ({ data, onChange }) => (
-  <TreeSelect
-    onChange={onChange}
-    treeData={data}
-    placeholder={<span>Search for a type...</span>}
-    style={{ width: 300 }}
-    dropdownStyle={{
-      maxHeight: 200,
-      overflow: 'auto',
-      zIndex: 1500,
-    }}
-    showSearch
-    allowClear
-    multiple
-    treeCheckable
-    treeDefaultExpandAll
-  />
+  <TreeSelectContainer className="rc-tree-select-open">
+    <TreeSelect
+      onChange={onChange}
+      treeData={data}
+      placeholder={<span>Search for a type...</span>}
+      dropdownStyle={{
+        maxHeight: 200,
+        overflow: 'auto',
+        zIndex: 1,
+      }}
+      showSearch
+      treeCheckable
+      showTreeIcon={false}
+      treeDefaultExpandAll
+      treeNodeFilterProp="title"
+    />
+  </TreeSelectContainer>
 )
 
 export default RCTreeSelect
