522be44fa1e6cece2937ceb5922521d5ca0a5dc8
laurenho025 <laurenh4@illinois.edu>
2021-10-06 22:35:36 -0500

    Work on keeping checked state and keep dropdown open by default

--- src/components/filter/RCTreeSelect.js
+++ src/components/filter/RCTreeSelect.js
@@ -7,6 +7,7 @@ import styled from 'styled-components/macro'
 const TreeSelectContainer = styled.div`
   .rc-tree-select {
     width: 100%;
+    height: 300px;
   }
   .rc-tree-select-selector {
     border-radius: 23px;
@@ -15,21 +16,27 @@ const TreeSelectContainer = styled.div`
 `
 
 const RCTreeSelect = ({ data, onChange }) => (
-  <TreeSelectContainer className="rc-tree-select-open">
+  <TreeSelectContainer>
     <TreeSelect
       onChange={onChange}
       treeData={data}
       placeholder={<span>Search for a type...</span>}
       dropdownStyle={{
-        maxHeight: 200,
+        height: 250,
+        width: 290,
         overflow: 'auto',
+        borderRadius: 7,
+        border: '1px solid #e0e1e2',
         zIndex: 1,
       }}
-      showSearch
+      dropdownMatchSelectWidth={false} // Allows for overflow-x
       treeCheckable
       showTreeIcon={false}
       treeDefaultExpandAll
-      treeNodeFilterProp="title"
+      treeNodeFilterProp="title" // Search the tree based on node titles instead of values
+      defaultOpen
+      defaultValue={data.filter((t) => t.checked)}
+      tagRender={() => null} // Prevent selected tags from showing
     />
   </TreeSelectContainer>
 )
--- src/utils/buildTypeSchema.js
+++ src/utils/buildTypeSchema.js
@@ -74,9 +74,8 @@ const addRCTreeSelectFields = (root, checkedTypes, showScientificNames) => {
   root.title = `${name} (${root.count})`
   // Value must be the type id because RCTreeSelect's onChange handler returns an array of the selected node's values
   root.value = `${root.id}`
-  // TODO: Figure out how to keep values expanded/checked in RCTreeSelect
-  // root.expanded = true
-  // root.checked = checkedTypes.length === 0 || checkedTypes.includes(root.id)
+  // Keep track of whether this type was checked so that selected state can be preserved across re-renders of the tree
+  root.checked = checkedTypes.length === 0 || checkedTypes.includes(root.value)
 
   for (const child of root.children) {
     addRCTreeSelectFields(child, checkedTypes, showScientificNames)
