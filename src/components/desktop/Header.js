077e32433681dddfe389596bccc50f35b798e164
riyajain59 <65252108+riyajain59@users.noreply.github.com>
2021-10-12 11:04:45 -0500

    🔗 Update header (#186)
    * Update changes of new header
    
    * added basic stylings
    
    * Fix header style issues
    
    * Clean up header code
    
    * fixed dropdown sass nesting bug
    
    * Implement dropdown component
    
    * Fix navlink styling
    
    * Update translation in text and formatting
    
    * Fix PR changes
    
    * Fix dropdown styling
    
    * Implement final PR changes
    
    * Make match const
    
    Co-authored-by: Riya Jain <riyajain@wirelessprv-10-192-167-80.near.illinois.edu>
    Co-authored-by: daniel-moon32 <dm32@illinois.edu>

--- src/components/desktop/Header.js
+++ src/components/desktop/Header.js
@@ -1,14 +1,17 @@
+import { CaretDown } from '@styled-icons/boxicons-regular'
 import { User } from '@styled-icons/boxicons-solid'
 import { useTranslation } from 'react-i18next'
+import { NavLink, useRouteMatch } from 'react-router-dom'
 import styled from 'styled-components/macro'
 
 const StyledHeader = styled.header`
-  height: 70px;
-  background-color: ${({ theme }) => theme.orange};
+  height: 56px;
+  background-color: ${({ theme }) => theme.background};
   display: flex;
   justify-content: space-between;
   align-items: center;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.12);
+  z-index: 1;
 
   img {
     height: 60%;
@@ -25,14 +28,52 @@ const StyledHeader = styled.header`
       height: 100%;
 
       li {
-        display: inline-grid;
-        place-items: center;
-        font-weight: bold;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         width: 110px;
         margin: 0;
         height: 100%;
-        color: ${({ theme }) => theme.background};
+        color: ${({ theme }) => theme.secondaryText};
         cursor: pointer;
+        position: relative;
+        font-family: Lato;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 14px;
+
+        .navbar {
+          display: block;
+          height: 100%;
+          width: 100%;
+        }
+
+        a {
+          text-decoration: none;
+          color: ${({ theme }) => theme.secondaryText};
+          text-align: center;
+        }
+
+        .active {
+          background-color: ${({ theme }) => theme.navBackground};
+          color: ${({ theme }) => theme.orange};
+          box-sizing: border-box;
+          height: 100%;
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          ::before {
+            content: '';
+            width: 100%;
+            position: absolute;
+            background-color: ${({ theme }) => theme.orange};
+            height: 3px;
+            bottom: 0;
+            left: 0;
+          }
+        }
 
         button {
           svg {
@@ -48,14 +89,72 @@ const StyledHeader = styled.header`
           font-family: inherit;
           font-weight: normal;
           cursor: pointer;
+          background-color: ${({ theme }) => theme.background};
         }
 
         button,
         &.active {
-          background-color: ${({ theme }) => theme.background};
           color: ${({ theme }) => theme.orange};
         }
       }
+    }
+  }
+`
+const Dropdown = ({ className, children, text }) => {
+  const match = useRouteMatch('/about/:slug')
+
+  return (
+    <div className={className}>
+      <div className={`button ${match && 'active'}`}>
+        {console.log(match)}
+        {text} <CaretDown height="8px" />
+      </div>
+      <div className="content">{children}</div>
+    </div>
+  )
+}
+
+const StyledDropdown = styled(Dropdown)`
+  display: inline-block;
+
+  .button {
+    background-color: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.secondaryText};
+    padding: 16px;
+    border: none;
+    height: 100%;
+    width: 100%;
+    padding: 19px 31px;
+
+    .active {
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  &:hover .content {
+    display: block;
+  }
+
+  .content {
+    display: none;
+    position: absolute;
+    background-color: ${({ theme }) => theme.background};
+    border-radius: 0px 0px 6px 6px;
+    box-shadow: rgba(0, 0, 0, 0.05) 0 15px 15px;
+    text-align: center;
+    min-width: 100%;
+    left: 0;
+
+    a {
+      color: ${({ theme }) => theme.secondaryText};
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+    }
+
+    .active ::before {
+      display: none;
     }
   }
 `
@@ -65,11 +164,35 @@ const Header = () => {
   const { t } = useTranslation()
   return (
     <StyledHeader>
-      <img src="/logo_white.svg" alt="Falling Fruit Logo" />
+      <img src="/logo_orange.svg" alt="Falling Fruit Logo" />
       <nav>
         <ul>
-          <li className="active">{t('Map')}</li>
-          <li>{t('About')}</li>
+          <li>
+            <NavLink to="/map" className="navbar" activeClassName="active">
+              {t('Map')}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/page2" className="navbar" activeClassName="active">
+              {t('Page 2')}
+            </NavLink>
+          </li>
+          <li>
+            <StyledDropdown text={t('About')}>
+              <NavLink to="/about/project" activeClassName="active">
+                {t('The project')}
+              </NavLink>
+              <NavLink to="/about/dataset" activeClassName="active">
+                {t('Imported datasets')}
+              </NavLink>
+              <NavLink to="/about/share" activeClassName="active">
+                {t('Sharing the harvest')}
+              </NavLink>
+              <NavLink to="/about/press" activeClassName="active">
+                {t('In the press')}
+              </NavLink>
+            </StyledDropdown>
+          </li>
           <li>
             <button>
               <User /> {t('Login')}
