0ed58319bb06c394d9a2449475b5984bcce43333
Daniel Moon <36148173+daniel-moon32@users.noreply.github.com>
2021-11-04 20:39:51 -0500

    🔐 Auth state integration (#196)
    * Add user api wrapper functions
    
    * implemented get JWT token given user and password
    
    * Attempt saving token into redux store
    
    * Remove accidental data table commit
    
    * Remove data table component from yarn.lock
    
    * Remove data table component from yarn.lock
    
    * Save token into redux store
    
    * Add checkbox saving to localstore and logout functionality
    
    * Add functionality to inputs
    
    * Add AuthInitializer
    
    * Style login page
    
    * finished login page
    
    * Add useEffect authToken to login
    
    * Handle invalid login
    
    * Refactor code
    
    * Style User
    
    * Fix PR comments
    
    * fix visual bug on dropdown
    
    Co-authored-by: Jeffrey Tang <jt12@illinois.edu>
    Co-authored-by: Arman Rafati <armanrafati@Armans-MacBook-Pro.local>
    Co-authored-by: Arman Rafati <armanrafati@wirelessprv-10-192-137-223.near.illinois.edu>
    Co-authored-by: code-arman <armanr123@gmail.com>

---
+++ src/utils/AuthInitializer.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { login } from '../redux/authSlice'
+
+const AuthInitializer = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(login())
+  }, [dispatch])
+  return null
+}
+
+export default AuthInitializer
