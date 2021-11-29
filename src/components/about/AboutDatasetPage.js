13c2c1426a50a3ddce33be13998ddd87ca54382f
Albert Xiao <axiao19502002@gmail.com>
2021-11-29 10:24:18 -0600

    style about page

--- src/components/about/AboutDatasetPage.js
+++ src/components/about/AboutDatasetPage.js
@@ -1,5 +1,6 @@
 import { Calendar } from '@styled-icons/boxicons-regular'
-import { Copyright, MapPin, Pin } from '@styled-icons/boxicons-solid'
+import { Copyright, MapPin } from '@styled-icons/boxicons-solid'
+import { LocationPin } from '@styled-icons/entypo/'
 import { useEffect, useState } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 
@@ -15,63 +16,73 @@ const AboutDatasetPage = () => {
   })
 
   const datasetID =
-    datasetIDMatch?.params.datasetID && parseInt(datasetIDMatch.params.entryId)
-
+    datasetIDMatch?.params.datasetID &&
+    parseInt(datasetIDMatch.params.datasetID)
+  console.log(datasetID)
   const [importData, setImportData] = useState()
-  //const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     async function fetchImportData() {
-      //setIsLoading(true)
+      setIsLoading(true)
 
-      setImportData(await getImportById(datasetID))
+      setImportData(await getImportById)
 
-      //      setIsLoading(false)
+      setIsLoading(false)
     }
 
     fetchImportData()
   }, [datasetID])
 
-  const createdAt = importData.created_at
-  const updatedAt = importData.updated_at
-  const name = importData.name
-  const url = importData.url
-  const comments = importData.comments
-  const license = importData.license
-  const muni = importData.muni
-  const locationCount = importData.location_count
   return (
-    <PageScrollWrapper>
-      <PageTemplate>
-        <h1>
-          #{datasetID} {name}
-        </h1>
-
-        <href a={url} />
-        <p>{comments}</p>
-        <IconBesideText>
-          <Pin color={theme.secondaryText} size={20} />
-          <p> {muni} </p>
-        </IconBesideText>
-        <IconBesideText>
-          <MapPin color={theme.secondaryText} size={20} />
-          <p> {locationCount} </p>
-        </IconBesideText>
-        <IconBesideText>
-          <Calendar color={theme.secondaryText} size={20} />
-          <p> {createdAt} </p>
-        </IconBesideText>
-        <IconBesideText>
-          <Calendar color={theme.secondaryText} size={20} />
-          <p> {updatedAt} </p>
-        </IconBesideText>
-        <IconBesideText>
-          <Copyright color={theme.secondaryText} size={20} />
-          <p> {license} </p>
-        </IconBesideText>
-        <p>Hi</p>
-      </PageTemplate>
-    </PageScrollWrapper>
+    !isLoading && (
+      <PageScrollWrapper>
+        <PageTemplate>
+          <h1 style={{ color: theme.orange, display: 'inline' }}>
+            #{datasetID}
+          </h1>{' '}
+          <h1 style={{ display: 'inline', padding: '10px' }}>
+            {importData.name}
+          </h1>
+          <br></br>
+          <br></br>
+          <a href={importData.url}>Website</a>
+          <p>{importData.comments}</p>
+          <IconBesideText>
+            <LocationPin color={theme.secondaryText} size={20} />
+            <p> {importData.muni} </p>
+          </IconBesideText>
+          <IconBesideText>
+            <MapPin color={theme.secondaryText} size={20} />
+            <p> {importData.location_count} Locations</p>
+          </IconBesideText>
+          <IconBesideText>
+            <Calendar color={theme.secondaryText} size={20} />
+            <p>
+              Created{' '}
+              {importData.created_at.substring(
+                0,
+                importData.created_at.indexOf('T'),
+              )}{' '}
+            </p>
+          </IconBesideText>
+          <IconBesideText>
+            <Calendar color={theme.secondaryText} size={20} />
+            <p>
+              Updated{' '}
+              {importData.updated_at.substring(
+                0,
+                importData.updated_at.indexOf('T'),
+              )}{' '}
+            </p>
+          </IconBesideText>
+          <IconBesideText>
+            <Copyright color={theme.secondaryText} size={20} />
+            <p> {importData.license} </p>
+          </IconBesideText>
+        </PageTemplate>
+      </PageScrollWrapper>
+    )
   )
 }
 
