6000b784542aa7ef541c760d191001ab68e7fbf4
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-12-28 13:42:10 -0500

    add distinguish user review

--- src/components/entry/EntryReviews.js
+++ src/components/entry/EntryReviews.js
@@ -1,19 +1,35 @@
+import { useSelector } from 'react-redux'
+
 import { ReviewForm } from '../form/ReviewForm'
 import { TextContent } from './Entry'
 import Review from './Review'
 
-const EntryReviews = ({ reviews, onImageClick, onReviewSubmit }) => (
-  <TextContent>
-    <h2>Reviews{reviews && ` (${reviews.length})`}</h2>
-    {reviews.map((review, index) => (
-      <Review
-        key={index}
-        review={review}
-        onImageClick={(imageIndex) => onImageClick(index, imageIndex)}
-      />
-    ))}
-    <ReviewForm onSubmit={onReviewSubmit} />
-  </TextContent>
-)
+const EntryReviews = ({ reviews, onImageClick, onReviewSubmit }) => {
+  const { user } = useSelector((state) => state.auth)
+
+  const userReviewIdx = reviews.findIndex(
+    (review) => review.user_id === user?.id,
+  )
+
+  let userReview
+  if (userReviewIdx !== -1) {
+    userReview = reviews.splice(userReviewIdx, 1)
+  }
+
+  return (
+    <TextContent>
+      <h2>Reviews{reviews && ` (${reviews.length})`}</h2>
+      {userReview && <Review review={userReview} />}
+      {reviews.map((review, index) => (
+        <Review
+          key={index}
+          review={review}
+          onImageClick={(imageIndex) => onImageClick(index, imageIndex)}
+        />
+      ))}
+      <ReviewForm onSubmit={onReviewSubmit} />
+    </TextContent>
+  )
+}
 
 export default EntryReviews
