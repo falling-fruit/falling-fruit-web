61b52d91c3ecdfd8dafdad50cb2a5e46747427d6
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-12-31 16:14:53 -0500

    add imports loader

--- src/components/table/DataTable.js
+++ src/components/table/DataTable.js
@@ -1,5 +1,56 @@
 import ReactDataTable from 'react-data-table-component'
+import Skeleton from 'react-loading-skeleton'
 import styled from 'styled-components/macro'
+
+const LoaderWrapper = styled.div`
+  width: 100%;
+  pointer-events: none;
+
+  span {
+    width: 100%;
+
+    .react-loading-skeleton {
+      display: block;
+      width: 100%;
+    }
+  }
+
+  .rdt {
+    &_TableRow {
+      display: flex;
+      padding: 16px 0;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+    }
+
+    &_TableCell {
+      flex: 1;
+      padding: 0 16px;
+    }
+  }
+`
+const TableLoader = () => (
+  <LoaderWrapper>
+    {new Array(10).fill(
+      <div className="rdt_TableRow">
+        <div className="rdt_TableCell">
+          <Skeleton />
+        </div>
+        <div className="rdt_TableCell">
+          <Skeleton />
+        </div>
+        <div className="rdt_TableCell">
+          <Skeleton />
+        </div>
+        <div className="rdt_TableCell">
+          <Skeleton />
+        </div>
+        <div className="rdt_TableCell">
+          <Skeleton />
+        </div>
+      </div>,
+    )}
+  </LoaderWrapper>
+)
 
 const TableWrapper = styled.div`
   .rdt {
@@ -10,18 +61,21 @@ const TableWrapper = styled.div`
 
     &_TableRow {
       font-size: 1rem;
+      ${({ $onRowClicked, theme }) =>
+        $onRowClicked &&
+        `
       cursor: pointer;
-    }
-
-    &_TableRow:hover {
-      background-color: ${({ theme }) => theme.secondaryBackground};
+      &:hover {
+        background-color: ${theme.secondaryBackground};
+      }
+      `}
     }
   }
 `
 
-const DataTable = (props) => (
-  <TableWrapper>
-    <ReactDataTable pagination {...props} />
+const DataTable = ({ ...props }) => (
+  <TableWrapper $onRowClicked={props.onRowClicked}>
+    <ReactDataTable progressComponent={<TableLoader />} pagination {...props} />
   </TableWrapper>
 )
 
