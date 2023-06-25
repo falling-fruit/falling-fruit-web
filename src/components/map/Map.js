3b1f0cd46904410ca00d3a3deff7fe13781926a8
ezwelty <ethan.welty@gmail.com>
2023-06-25 16:21:23 +0200

    Hide points of interest from map by default

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -190,9 +190,17 @@ const Map = ({
           disableDefaultUI: true,
           // TODO: should we disable tilt?
           // tilt: 0,
+          // Toggle all basemap icons
+          // https://developers.google.com/maps/documentation/javascript/style-reference
           styles: [
             {
-              featureType: 'poi.business',
+              featureType: 'poi',
+              elementType: 'labels.icon',
+              stylers: [{ visibility: showBusinesses ? 'on' : 'off' }],
+            },
+            {
+              featureType: 'landscape',
+              elementType: 'labels.icon',
               stylers: [{ visibility: showBusinesses ? 'on' : 'off' }],
             },
           ],
