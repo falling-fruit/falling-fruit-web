89502bf1e7634fd79daa86dbf89bb96cea9f4ccc
ezwelty <ethan.welty@gmail.com>
2023-07-04 22:36:11 +0200

    Initialize Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'react-toastify/dist/ReactToastify.css'
 
 import { WindowSize } from '@reach/window-size'
+import ReactGA from 'react-ga4'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
@@ -11,6 +12,10 @@ import Toast from './components/ui/Toast'
 import { store } from './redux/store'
 import AuthInitializer from './utils/AuthInitializer'
 import { ConnectedBreakpoint, useIsDesktop } from './utils/useBreakpoint'
+
+if (process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID)
+}
 
 const App = () => (
   <Provider store={store}>
