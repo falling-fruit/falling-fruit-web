18206dbd3ff261a1956c3724a141b98e9a21d31c
ezwelty <ethan.welty@gmail.com>
2023-07-05 08:35:22 +0200

    Load Google Analytics with HTML script tag

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import 'react-toastify/dist/ReactToastify.css'
 
 import { WindowSize } from '@reach/window-size'
-import ReactGA from 'react-ga4'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import MainPage from './components/MainPage'
-import RouteTracker from './components/tracking/RouteTracker'
 import GlobalStyle, { theme } from './components/ui/GlobalStyle'
 import Toast from './components/ui/Toast'
 import { store } from './redux/store'
 import AuthInitializer from './utils/AuthInitializer'
 import { ConnectedBreakpoint, useIsDesktop } from './utils/useBreakpoint'
-
-if (process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID) {
-  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID)
-}
 
 const App = () => (
   <Provider store={store}>
@@ -28,7 +22,6 @@ const App = () => (
       rtl={false}
     />
     <BrowserRouter>
-      <RouteTracker />
       <ThemeProvider theme={theme}>
         <Switch>
           <Route exact path="/">
