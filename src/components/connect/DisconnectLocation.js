f50972a066a338d756b2d16ce6d2c9c6cc1603a1
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2024-07-11 22:34:14 +0100

    Bugfix: edit/position -> list and back should not reset position

--- src/components/connect/DisconnectLocation.js
+++ src/components/connect/DisconnectLocation.js
@@ -1,14 +1,21 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { clearLocation } from '../../redux/locationSlice'
+import { useIsDesktop } from '../../utils/useBreakpoint'
 
 const DisconnectLocation = () => {
   const dispatch = useDispatch()
+  const isBeingEdited = useSelector((state) => state.location.isBeingEdited)
+  const isDesktop = useIsDesktop()
 
   useEffect(() => {
-    dispatch(clearLocation())
-  }, [dispatch])
+    // on mobile, we offer viewing the list when editing position
+    // don't reset editing position for someone who tries to do it
+    if (isDesktop || !isBeingEdited) {
+      dispatch(clearLocation())
+    }
+  }, [dispatch]) //eslint-disable-line
 
   return null
 }
