700bd17eda01747ffaddaaaafbf381b115237184
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2024-07-11 21:30:11 +0100

    Bugfix: do not center on location after page refresh
    Also don't endlessly setView through ConnectView

--- src/components/connect/ConnectLocation.js
+++ src/components/connect/ConnectLocation.js
@@ -6,13 +6,13 @@ import {
   setIsBeingEditedAndResetPosition,
 } from '../../redux/locationSlice'
 import { setView } from '../../redux/mapSlice'
-import { currentPathWithView } from '../../utils/appUrl'
+import { currentPathWithView, parseCurrentUrl } from '../../utils/appUrl'
 import { useAppHistory } from '../../utils/useAppHistory'
 import { useIsDesktop } from '../../utils/useBreakpoint'
 
 const ConnectLocation = ({ locationId, isBeingEdited }) => {
   const dispatch = useDispatch()
-  const view = useSelector((state) => state.map.view)
+  const viewRedux = useSelector((state) => state.map.view)
   const position = useSelector((state) => state.location.position)
   const history = useAppHistory()
   const isDesktop = useIsDesktop()
@@ -20,24 +20,19 @@ const ConnectLocation = ({ locationId, isBeingEdited }) => {
   useEffect(() => {
     dispatch(fetchLocationData({ locationId, isBeingEdited })).then(
       (action) => {
-        if (action.payload && !view) {
-          // TODO: the view could be null because it wasn't set yet but present in coords
-          // use parseCurrentUrl instead
-          // old comment:
-          // If the view is null
-          // (e.g. we navigated to /locations/:locationId in the browser)
-          // set it to the centre of the retrieved location
-          const newView = {
+        if (action.payload && !viewRedux) {
+          const { view: viewUrl } = parseCurrentUrl()
+          const view = viewUrl || {
             center: {
               lat: action.payload.lat,
               lng: action.payload.lng,
             },
             zoom: 16,
           }
-          dispatch(setView(newView))
-          // and navigate to the page with the new URL
+          dispatch(setView(view))
+          // navigate to the page with the new URL
           // to trigger component reload
-          const newUrl = currentPathWithView(newView)
+          const newUrl = currentPathWithView(view)
           history.push(newUrl)
         }
       },
@@ -52,13 +47,14 @@ const ConnectLocation = ({ locationId, isBeingEdited }) => {
     if (isBeingEdited && position && !isDesktop) {
       // On mobile, we need to center the map on the edited location
       // because the UX involves panning the map under a central pin
+      // Todo how to allow roundtrip to settings?:w
       dispatch(
         setView({
           center: {
             lat: position.lat,
             lng: position.lng,
           },
-          zoom: Math.max(view ? view.zoom : 0, 16),
+          zoom: Math.max(viewRedux ? viewRedux.zoom : 0, 16),
         }),
       )
     }
