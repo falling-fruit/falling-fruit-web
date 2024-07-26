16bdbd2ff60df37c4ca642ad081bb8b4e5dd0b1b
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2024-07-26 14:49:16 +0100

    Fix: restore dropping the new location pin on mobile

--- src/components/connect/DisconnectLocation.js
+++ src/components/connect/DisconnectLocation.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { clearLocation } from '../../redux/locationSlice'
 import { useIsDesktop } from '../../utils/useBreakpoint'
@@ -7,12 +7,14 @@ import { useIsDesktop } from '../../utils/useBreakpoint'
 const DisconnectLocation = () => {
   const dispatch = useDispatch()
   const isDesktop = useIsDesktop()
+  const { locationId } = useSelector((state) => state.location)
 
   useEffect(() => {
     // on mobile, we offer viewing the list when editing position or viewing panorama
     // so we don't reset editing position for someone who tries to do it
-    //  instead, the Map button takes them to those pages
-    if (isDesktop) {
+    // - instead, the Map button takes them to those pages
+    // we only want to drop the new location pin from map view
+    if (isDesktop || locationId === 'new') {
       dispatch(clearLocation())
     }
   }, [dispatch]) //eslint-disable-line
