86a558520848c465d01c88fcdbd2a077917c959b
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2024-08-15 10:12:40 +0100

    Bugfix: clear location on mobile too
    I think the reasoning in the comment applied when /list also triggered the disconnect but I think it shouldn't

--- src/components/connect/DisconnectLocation.js
+++ src/components/connect/DisconnectLocation.js
@@ -2,22 +2,16 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { clearLocation } from '../../redux/locationSlice'
-import { useIsDesktop } from '../../utils/useBreakpoint'
 
 const DisconnectLocation = () => {
   const dispatch = useDispatch()
-  const isDesktop = useIsDesktop()
   const { locationId } = useSelector((state) => state.location)
 
   useEffect(() => {
-    // on mobile, we offer viewing the list when editing position or viewing panorama
-    // so we don't reset editing position for someone who tries to do it
-    // - instead, the Map button takes them to those pages
-    // we only want to drop the new location pin from map view
-    if (isDesktop || locationId === 'new') {
+    if (locationId) {
       dispatch(clearLocation())
     }
-  }, [dispatch]) //eslint-disable-line
+  }, [dispatch, locationId])
 
   return null
 }
