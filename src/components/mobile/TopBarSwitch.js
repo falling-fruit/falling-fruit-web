8254247f6483c55d4c130584c66b8c47c0f630ff
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2024-09-16 10:23:57 +0100

    Bugfix: only show filter for /map, /list, and location page
    We could use redux state here but fully open drawer goes on top of the search box

--- src/components/mobile/TopBarSwitch.js
+++ src/components/mobile/TopBarSwitch.js
@@ -1,48 +1,31 @@
-import { Route, Switch, useLocation } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Search from '../search/Search'
 import TopBar from '../ui/TopBar'
 import FilterWrapper from './FilterWrapper'
 import LocationNav from './LocationNav'
 
-const TopBarSwitch = () => {
-  const { state } = useLocation()
-  const isFromList = state?.fromPage === '/list'
-
-  return (
-    <Switch>
-      <Route
-        path={[
-          '/settings',
-          '/users/edit',
-          '/about',
-          '/users/sign_up',
-          '/users/sign_in',
-          '/users/password',
-          '/users/confirmation',
-        ]}
-      ></Route>
-      <Route
-        path={[
-          '/reviews/:reviewId/edit',
-          '/locations/:locationId/review',
-          '/locations/:locationId/edit',
-          '/locations/new',
-        ]}
-      >
-        <TopBar>
-          <LocationNav />
-        </TopBar>
-      </Route>
-      {isFromList && <Route path="/locations/:locationId" />}
-      <Route>
-        <TopBar>
-          <Search />
-          <FilterWrapper />
-        </TopBar>
-      </Route>
-    </Switch>
-  )
-}
+const TopBarSwitch = () => (
+  <Switch>
+    <Route
+      path={[
+        '/reviews/:reviewId/edit',
+        '/locations/:locationId/review',
+        '/locations/:locationId/edit',
+        '/locations/new',
+      ]}
+    >
+      <TopBar>
+        <LocationNav />
+      </TopBar>
+    </Route>
+    <Route path={['/map', '/list', '/locations/:locationId']}>
+      <TopBar>
+        <Search />
+        <FilterWrapper />
+      </TopBar>
+    </Route>
+  </Switch>
+)
 
 export default TopBarSwitch
