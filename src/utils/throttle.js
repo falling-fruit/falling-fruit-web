7da998affc311111fc366ba434a137c80e9ed5df
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2024-10-01 10:38:00 +0100

    Do not reset bounds on resize + throttle map update (#454)
    * Remove argument to google-map-react to reset bounds on map resize
    * Throttle handleViewChange (which calls API, etc)

---
+++ src/utils/throttle.js
@@ -0,0 +1,28 @@
+// source:
+// https://github.com/sindresorhus/throttleit/blob/main/index.js
+// it requires node 18 so paste it in
+function throttle(function_, wait) {
+  let timeoutId
+  let lastCallTime = 0
+
+  return function throttled(...arguments_) {
+    // eslint-disable-line func-names
+    clearTimeout(timeoutId)
+
+    const now = Date.now()
+    const timeSinceLastCall = now - lastCallTime
+    const delayForNextCall = wait - timeSinceLastCall
+
+    if (delayForNextCall <= 0) {
+      lastCallTime = now
+      function_.apply(this, arguments_)
+    } else {
+      timeoutId = setTimeout(() => {
+        lastCallTime = Date.now()
+        function_.apply(this, arguments_)
+      }, delayForNextCall)
+    }
+  }
+}
+
+module.exports = throttle
