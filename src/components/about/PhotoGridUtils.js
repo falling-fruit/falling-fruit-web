18deada1138ee39b6c3aff312d747a6ed24f8224
Marcelo Amorim <mamorim813@gmail.com>
2024-10-04 22:47:07 -0300

    Fix layout issues around images on the About page for narrow screens (#471):
    - Updated image grid to span full width on mobile.
    - In the staff section, images remain aligned to the left, with text on the right. When the text exceeds the image height, it wraps beneath and occupies full width for better readability.

--- src/components/about/PhotoGridUtils.js
+++ src/components/about/PhotoGridUtils.js
@@ -2,25 +2,28 @@ import styled from 'styled-components/macro'
 
 const Grid = styled.div`
   @media ${({ theme }) => theme.device.mobile} {
-    grid-template-columns: 160px 160px;
+    grid-template-columns: repeat(2, 1fr);
     gap: 6px;
     margin-top: 17px;
     margin-bottom: 17px;
+    justify-content: center;
+    float: 'top';
   }
   @media ${({ theme }) => theme.device.desktop} {
     margin: 16px;
     gap: 8px;
     grid-template-columns: 156px auto;
+    float: ${(props) => props.float || 'right'};
   }
   color: ${({ theme }) => theme.secondaryText};
-  float: ${(props) => props.float || 'right'};
   display: grid;
 `
 
 const Image = styled.img`
   @media ${({ theme }) => theme.device.mobile} {
-    width: 160px;
-    height: 94px;
+    width: 100%;
+    max-height: 250px;
+    height: auto;
   }
   @media ${({ theme }) => theme.device.desktop} {
     width: 156px;
