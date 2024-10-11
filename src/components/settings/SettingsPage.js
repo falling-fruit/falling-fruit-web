efa9e97a2033677b10e2ae0e0ff52316784aa97e
ezwelty <ethan.welty@gmail.com>
2024-10-11 10:22:45 +0200

    Merge style changes into single div

--- src/components/settings/SettingsPage.js
+++ src/components/settings/SettingsPage.js
@@ -220,72 +220,61 @@ const SettingsPage = ({ desktop }) => {
           GoogleMapTypes.includes(settings.mapType)
             ? {}
             : {
+                opacity: '50%',
+                filter: 'grayscale(50%)',
                 pointerEvents: 'none',
               }
         }
       >
-        <div
-          style={
-            GoogleMapTypes.includes(settings.mapType)
-              ? {}
-              : {
-                  opacity: '50%',
-                  filter: 'grayscale(50%)',
-                }
-          }
-        >
-          <RadioTiles
-            options={[
-              {
-                label: t('side_menu.bicycle'),
-                value: 'BicyclingLayer',
-                image: GoogleBicycling,
-              },
-              {
-                label: t('side_menu.transit'),
-                value: 'TransitLayer',
-                image: GoogleTransit,
-              },
-            ]}
-            value={
-              settings.mapLayers.length === 0 ? null : settings.mapLayers[0]
-            }
-            onChange={(value) => {
-              if (value === settings.mapLayers[0]) {
-                value = null
-              }
-              dispatch(
-                updateSettings({
-                  mapLayers: value ? [value] : [],
-                }),
-              )
-            }}
-          />
-          {[
+        <RadioTiles
+          options={[
             {
-              field: 'showBusinesses',
-              label: t('poi'),
+              label: t('side_menu.bicycle'),
+              value: 'BicyclingLayer',
+              image: GoogleBicycling,
             },
-          ].map(({ field, label }) => (
-            <LabeledRow
-              key={field}
-              left={
-                <Checkbox
-                  id={field}
-                  onClick={(e) =>
-                    dispatch(
-                      updateSettings({
-                        [field]: e.target.checked,
-                      }),
-                    )
-                  }
-                  checked={settings[field]}
-                />
-              }
-              label={<label htmlFor={field}>{label}</label>}
-            />
-          ))}
-        </div>
+            {
+              label: t('side_menu.transit'),
+              value: 'TransitLayer',
+              image: GoogleTransit,
+            },
+          ]}
+          value={settings.mapLayers.length === 0 ? null : settings.mapLayers[0]}
+          onChange={(value) => {
+            if (value === settings.mapLayers[0]) {
+              value = null
+            }
+            dispatch(
+              updateSettings({
+                mapLayers: value ? [value] : [],
+              }),
+            )
+          }}
+        />
+        {[
+          {
+            field: 'showBusinesses',
+            label: t('poi'),
+          },
+        ].map(({ field, label }) => (
+          <LabeledRow
+            key={field}
+            left={
+              <Checkbox
+                id={field}
+                onClick={(e) =>
+                  dispatch(
+                    updateSettings({
+                      [field]: e.target.checked,
+                    }),
+                  )
+                }
+                checked={settings[field]}
+              />
+            }
+            label={<label htmlFor={field}>{label}</label>}
+          />
+        ))}
       </div>
 
       <h5>OpenStreetMap</h5>
