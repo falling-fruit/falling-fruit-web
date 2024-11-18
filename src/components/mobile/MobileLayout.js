77a4597b1c79138a771aab47d3f59c3433a2db9c
Julien Acone <96042387+derkiz@users.noreply.github.com>
2024-11-18 18:12:14 +0100

    Removes: Empty white bar at the bottom of location and review forms #601
    Issue #601 
    Removes unnecessary bottom padding

--- src/components/mobile/MobileLayout.js
+++ src/components/mobile/MobileLayout.js
@@ -85,7 +85,7 @@ const MobileLayout = () => {
         <MapPage />
       </div>
       {connectRoutes}
-      <TabPanels style={{ paddingBottom: '50px' }}>
+      <TabPanels}>
         <Switch>
           {aboutRoutes}
           {authRoutes}
