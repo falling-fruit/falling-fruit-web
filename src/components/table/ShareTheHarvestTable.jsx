974074e80edb1256ce41b3817daa77886fef2dad
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2024-12-06 16:53:18 +0000

    Localise share the harvest table

--- src/components/table/ShareTheHarvestTable.jsx
+++ src/components/table/ShareTheHarvestTable.jsx
@@ -1,6 +1,7 @@
 import { Facebook, Instagram } from '@styled-icons/boxicons-logos'
 import { Search as SearchIcon } from '@styled-icons/boxicons-regular'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import styled from 'styled-components/macro'
 
 import harvestData from '../../constants/data/harvest.json'
@@ -71,54 +72,79 @@ const FormattedSocials = ({ facebook, instagram, x }) => {
   )
 }
 
-const columns = [
-  {
-    id: 'country',
-    name: 'Country',
-    selector: (row) => row.country ?? '-',
-    sortable: true,
-    wrap: true,
-  },
-  {
-    id: 'state',
-    name: 'State',
-    selector: (row) => row.state ?? '-',
-    sortable: true,
-    wrap: true,
-  },
-  {
-    id: 'city',
-    name: 'City',
-    selector: (row) => row.city ?? '-',
-    sortable: true,
-    wrap: true,
-  },
-  {
-    id: 'name',
-    name: 'Name',
-    selector: (row) => row.name,
-    sortable: true,
-    grow: 2.5,
-    format: FormattedOrganization,
-    wrap: true,
-  },
-  {
-    id: 'social',
-    name: 'Social',
-    selector: (row) => row.facebook || row.instagram || row.x,
-    format: FormattedSocials,
-    compact: true,
-    width: '80px',
-    right: true,
-  },
-]
-
 const ShareTheHarvestTable = () => {
   const [filterText, setFilterText] = React.useState('')
+  const { t } = useTranslation()
+
+  const translatedCountries = {
+    Australia: t('glossary.australia'),
+    Belgium: t('glossary.belgium'),
+    Brazil: t('glossary.brazil'),
+    Canada: t('glossary.canada'),
+    Chile: t('glossary.chile'),
+    Czechia: t('glossary.czechia'),
+    Fiji: t('glossary.fiji'),
+    Finland: t('glossary.finland'),
+    France: t('glossary.france'),
+    Germany: t('glossary.germany'),
+    Greece: t('glossary.greece'),
+    Ireland: t('glossary.ireland'),
+    Italy: t('glossary.italy'),
+    'New Zealand': t('glossary.new_zealand'),
+    Portugal: t('glossary.portugal'),
+    Spain: t('glossary.spain'),
+    Sweden: t('glossary.sweden'),
+    Switzerland: t('glossary.switzerland'),
+    'United Kingdom': t('glossary.united_kingdom'),
+    'United States': t('glossary.united_states'),
+  }
+
+  const columns = [
+    {
+      id: 'country',
+      name: 'Country',
+      selector: (row) => translatedCountries[row.country] || row.country || '-',
+      sortable: true,
+      wrap: true,
+    },
+    {
+      id: 'state',
+      name: 'State',
+      selector: (row) => row.state ?? '-',
+      sortable: true,
+      wrap: true,
+    },
+    {
+      id: 'city',
+      name: 'City',
+      selector: (row) => row.city ?? '-',
+      sortable: true,
+      wrap: true,
+    },
+    {
+      id: 'name',
+      name: 'Name',
+      selector: (row) => row.name,
+      sortable: true,
+      grow: 2.5,
+      format: FormattedOrganization,
+      wrap: true,
+    },
+    {
+      id: 'social',
+      name: 'Social',
+      selector: (row) => row.facebook || row.instagram || row.x,
+      format: FormattedSocials,
+      compact: true,
+      width: '80px',
+      right: true,
+    },
+  ]
 
   const filteredData = harvestData.filter((item) => {
     const query = filterText.toLowerCase()
-    const keywords = [item.country, item.state, item.city, item.name]
+    const translatedCountry = translatedCountries[item.country] || item.country
+    const keywords = [translatedCountry, item.state, item.city, item.name]
     return keywords.some(
       (keyword) => keyword && keyword.toLowerCase().includes(query),
     )
