d80dbffe6db6998d86106d378aecc6e260ae4974
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2025-02-01 11:45:10 +0000

    Hack for when key ends with _html and i18nViz is keys

--- src/components/connect/ConnectSearchParams.js
+++ src/components/connect/ConnectSearchParams.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
 import i18n from 'i18next'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
@@ -10,17 +10,26 @@ const ConnectSearchParams = () => {
   const location = useLocation()
   const history = useAppHistory()
   const searchParams = new URLSearchParams(location.search)
+
+  const escapeHtml = useMemo(
+    () => (unsafe) => unsafe
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;'),
+    [],
+  )
   const i18nViz = searchParams.get('i18n_viz')
 
   useEffect(() => {
     if (i18nViz) {
       const originalT = i18n.t.bind(i18n)
       i18n.t = (key, options) => {
-        // Convert any option values that are objects with props.title to strings
         if (options) {
           Object.entries(options).forEach(([k, v]) => {
             if (v && typeof v === 'object' && v.props?.title) {
-              options[k] = v.props.title
+              options[k] = `[${v.props.title}]`
             }
           })
         }
@@ -34,32 +43,37 @@ const ConnectSearchParams = () => {
         ) {
           return `Localised text, key: ${key}, value: ${translation}`
         }
-        if (i18nViz === 'keys') {
-          const x = translation
-          translation = key
-          key = x
-        }
 
         if (key.endsWith('_html')) {
+          if (i18nViz === 'keys') {
+            const x = escapeHtml(translation)
+            translation = key
+            key = x
+          }
           return `<span style="background-color: gold; cursor: pointer;" title="${key}" onclick="alert('Copied key for HTML element to clipboard: ${key}'); navigator.clipboard.writeText('${key}')">${translation}</span>`
+        } else {
+          if (i18nViz === 'keys') {
+            const x = translation
+            translation = key
+            key = x
+          }
+          return (
+            <span
+              style={{
+                backgroundColor: 'yellow',
+                position: 'relative',
+                cursor: 'pointer',
+              }}
+              title={key}
+              onClick={() => {
+                navigator.clipboard.writeText(key)
+                toast.info(`Copied to clipboard: ${key}`)
+              }}
+            >
+              {translation}
+            </span>
+          )
         }
-        return (
-          <span
-            style={{
-              backgroundColor: 'yellow',
-              position: 'relative',
-              cursor: 'pointer',
-            }}
-            title={key}
-            onClick={() => {
-              console.log({ i18nViz, translation, key, options })
-              navigator.clipboard.writeText(key)
-              toast.info(`Copied to clipboard: ${key}`)
-            }}
-          >
-            {translation}
-          </span>
-        )
       }
       history.removeParam('i18n_viz')
     }
