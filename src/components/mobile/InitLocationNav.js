16f4a5557d11b557ee79339190fcf9d27024885b
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2025-03-07 16:39:20 +0000

    Disallow submitting same location twice

--- src/components/mobile/InitLocationNav.js
+++ src/components/mobile/InitLocationNav.js
@@ -1,8 +1,10 @@
 import { Check, X } from '@styled-icons/boxicons-regular'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
 import styled from 'styled-components/macro'
 
+import { parseCurrentUrl } from '../../utils/appUrl'
 import { useAppHistory } from '../../utils/useAppHistory'
 import { theme } from '../ui/GlobalStyle'
 import IconButton from '../ui/IconButton'
@@ -15,7 +17,31 @@ const Instructions = styled.span`
 const InitLocationNav = () => {
   const { t } = useTranslation()
   const history = useAppHistory()
+  const { locations } = useSelector((state) => state.map)
   const { form } = useSelector((state) => state.location)
+  const { view } = parseCurrentUrl()
+
+  const isTooCloseToExistingLocation = () => {
+    if (!view.center || !locations || locations.length === 0) {
+      return false
+    }
+
+    return locations.some((location) => {
+      const latDiff = Math.abs(location.lat - view.center.lat)
+      const lngDiff = Math.abs(location.lng - view.center.lng)
+      return latDiff + lngDiff < 1e-5
+    })
+  }
+
+  const tooClose = isTooCloseToExistingLocation()
+
+  const handleConfirmClick = () => {
+    if (tooClose) {
+      toast.warning(t('locations.init.position_too_close'))
+      return
+    }
+    history.push('/locations/new')
+  }
 
   return (
     <TopBarNav
@@ -41,7 +67,11 @@ const InitLocationNav = () => {
             raised
             size={54}
             color={theme.green}
-            onClick={() => history.push('/locations/new')}
+            onClick={handleConfirmClick}
+            style={{
+              opacity: tooClose ? 0.5 : 1,
+              cursor: tooClose ? 'help' : 'pointer',
+            }}
           />
         </>
       }
