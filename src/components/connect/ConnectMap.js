93f2d7e2fc9087aabf26873c96395e49c35b4997
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2025-03-17 12:38:43 +0000

    Complete the two way binding for map

--- src/components/connect/ConnectMap.js
+++ src/components/connect/ConnectMap.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { setInitialView } from '../../redux/mapSlice'
 import { parseCurrentUrl } from '../../utils/appUrl'
+import { useAppHistory } from '../../utils/useAppHistory'
 
 const DEFAULT_LAT = 40.1125785
 const DEFAULT_LNG = -88.2287926
@@ -10,9 +11,10 @@ const DEFAULT_ZOOM = 4
 
 const ConnectMap = () => {
   const dispatch = useDispatch()
-  const { initialView } = useSelector((state) => state.map)
+  const { googleMap, initialView } = useSelector((state) => state.map)
   const { lastMapView } = useSelector((state) => state.viewport)
   const hasInitialView = !!initialView
+  const history = useAppHistory()
   const parsedUrl = parseCurrentUrl()
   const view = parsedUrl.view ||
     lastMapView || {
@@ -25,6 +27,15 @@ const ConnectMap = () => {
       dispatch(setInitialView(view))
     }
   }, [dispatch, hasInitialView]) //eslint-disable-line
+
+  useEffect(() => {
+    if (hasInitialView && !parsedUrl.view) {
+      history.replaceView({
+        center: googleMap.getCenter().toJSON(),
+        zoom: googleMap.getZoom(),
+      })
+    }
+  }, [dispatch, hasInitialView, !!parsedUrl.view]) //eslint-disable-line
 
   return null
 }
