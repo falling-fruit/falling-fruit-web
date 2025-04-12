043697c6091570d8c0e715495f0737bedd14b891
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2025-04-12 15:29:27 +0100

    XXX On expired access token, go to sign in page
    XXX to test, set in console:
    localStorage.authToken = 'test'
    localStorage.refreshToken = 'test'

--- src/utils/AuthInitializer.js
+++ src/utils/AuthInitializer.js
@@ -1,14 +1,35 @@
+import i18next from 'i18next'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { toast } from 'react-toastify'
 
 import { checkAuth } from '../redux/authSlice'
+import { pathToSignInPage } from './appUrl'
+import { useAppHistory } from './useAppHistory'
 
 const AuthInitializer = () => {
   const dispatch = useDispatch()
+  const history = useAppHistory()
 
   useEffect(() => {
     dispatch(checkAuth())
-  }, [dispatch])
+      .unwrap()
+      .then(([_user, error]) => {
+        console.log(_user, error)
+        if (error) {
+          if (error.message === 'Invalid refresh token') {
+            history.push(pathToSignInPage())
+          } else {
+            toast.error(
+              i18next.t('error_message.auth.check_failed', {
+                message:
+                  error.message || i18next.t('error_message.unknown_error'),
+              }),
+            )
+          }
+        }
+      })
+  }, [dispatch, history])
 
   return null
 }
