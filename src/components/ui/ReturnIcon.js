f7d9c299ccaa8e5436394742ec7ee39642715c58
ezwelty <ethan.welty@gmail.com>
2025-04-28 03:14:12 +0200

    Rotate ArrowBack icon for RTL

--- src/components/ui/ReturnIcon.js
+++ src/components/ui/ReturnIcon.js
@@ -1,14 +1,16 @@
-import {
-  ArrowBack,
-  RightArrowAlt as ArrowForward,
-} from '@styled-icons/boxicons-regular'
+import { ArrowBack } from '@styled-icons/boxicons-regular'
 import { useTranslation } from 'react-i18next'
 
 const ReturnIcon = (props) => {
   const { i18n } = useTranslation()
   const isRTL = i18n.dir() === 'rtl'
 
-  return isRTL ? <ArrowForward {...props} /> : <ArrowBack {...props} />
+  return (
+    <ArrowBack
+      style={{ transform: `rotate(${isRTL ? 180 : 0}deg)` }}
+      {...props}
+    />
+  )
 }
 
 export default ReturnIcon
