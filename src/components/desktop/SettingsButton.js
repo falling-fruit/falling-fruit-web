76d3508488a383a82bd30bbe40a5e688827a5df5
ezwelty <ethan.welty@gmail.com>
2025-04-28 03:02:16 +0200

    Use logical positioning in SettingsButton

--- src/components/desktop/SettingsButton.js
+++ src/components/desktop/SettingsButton.js
@@ -9,12 +9,10 @@ const StyledSettingsButton = styled.button`
   cursor: pointer;
   padding: 5px 0;
   border: none;
-  border-top: 1px solid ${({ theme }) => theme.secondaryBackground};
+  border-block-start: 1px solid ${({ theme }) => theme.secondaryBackground};
   position: sticky;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
+  inset-block-end: 0;
+  inset-inline: 0;
   min-height: 3.5em;
   background: ${({ theme }) => theme.background};
   display: flex;
@@ -29,56 +27,33 @@ const PrimaryText = styled.div`
   color: ${({ theme }) => theme.headerText};
 `
 
-const BaseSettingsCircleIcon = styled(Cog)`
+const SettingsCircleIcon = styled(Cog)`
   width: 1.75em;
+  margin-inline-start: 1.5em;
+  margin-inline-end: 1em;
 `
 
-const LTRSettingsCircleIcon = styled(BaseSettingsCircleIcon)`
-  margin-left: 1.5em;
-  margin-right: 1em;
-`
-
-const RTLSettingsCircleIcon = styled(BaseSettingsCircleIcon)`
-  margin-left: 1em;
-  margin-right: 1.5em;
-`
-
-const BaseChevronIcon = styled.div`
+const ChevronIcon = styled.div`
   flex: 1 1 0;
   display: flex;
-`
-
-export const LTRChevronIcon = styled(BaseChevronIcon)`
-  justify-content: right;
-  align-items: right;
-  margin-right: 1em;
-  margin-left: 0;
-`
-
-export const RTLChevronIcon = styled(BaseChevronIcon)`
-  justify-content: left;
-  align-items: left;
-  margin-right: 0;
-  margin-left: 1em;
+  justify-content: end;
+  align-items: end;
+  margin-inline-end: 1em;
+  margin-inline-start: 0;
 `
 
 const SettingsButton = ({ onClick }) => {
   const { t, i18n } = useTranslation()
   const isRTL = i18n.dir() === 'rtl'
-
-  const SettingsCircleIcon = isRTL
-    ? RTLSettingsCircleIcon
-    : LTRSettingsCircleIcon
-  const DirectionalChevronIcon = isRTL ? RTLChevronIcon : LTRChevronIcon
   const ChevronComponent = isRTL ? ChevronLeft : ChevronRight
 
   return (
     <StyledSettingsButton onClick={onClick}>
       <SettingsCircleIcon color={theme.orange} />
       <PrimaryText>{t('menu.settings')}</PrimaryText>
-      <DirectionalChevronIcon>
+      <ChevronIcon>
         <ChevronComponent width="1.75em" color={theme.orange} />
-      </DirectionalChevronIcon>
+      </ChevronIcon>
     </StyledSettingsButton>
   )
 }
