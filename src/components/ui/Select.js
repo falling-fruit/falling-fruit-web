fae9d24e1ab9b84af6333a8ed0cc9a467e4ec1ef
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2025-05-21 22:17:15 +0100

    Toggle location types on/off in dropdown selector (#789)
    * Fix crash when typing e.g. 'raspberry' twice
    * Show already selected elements in the types menu
    * Selecting a selected type deselects it

--- src/components/ui/Select.js
+++ src/components/ui/Select.js
@@ -79,6 +79,10 @@ const SelectParent = styled.div`
     display: flex;
     align-items: center;
   }
+  .select__option--is-selected {
+    background-color: ${({ theme }) =>
+      transparentize(0.5, theme.transparentOrange)};
+  }
 `
 const StyledSelect = SelectParent.withComponent(Select)
 const StyledCreatableSelect = SelectParent.withComponent(Creatable)
@@ -114,8 +118,8 @@ const InfiniteMenuList = ({ children }) => {
 
   return (
     <div style={{ overflowY: 'auto' }}>
-      {children.slice(0, visibleCount)}
-      {visibleCount < children.length && (
+      {Array.isArray(children) ? children.slice(0, visibleCount) : children}
+      {Array.isArray(children) && visibleCount < children.length && (
         <div ref={loadMoreRef} style={{ height: 1 }} />
       )}
     </div>
