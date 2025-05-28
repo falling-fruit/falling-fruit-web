625d62aea141e6d8292130b47a7e517d7e504bc5
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2025-05-28 12:20:24 +0100

    Move the CSS from the global styling to individual component

--- src/components/ui/Select.js
+++ src/components/ui/Select.js
@@ -79,10 +79,6 @@ const SelectParent = styled.div`
     display: flex;
     align-items: center;
   }
-  .select__option--is-selected {
-    background-color: ${({ theme }) =>
-      transparentize(0.5, theme.transparentOrange)};
-  }
 `
 const StyledSelect = SelectParent.withComponent(Select)
 const StyledCreatableSelect = SelectParent.withComponent(Creatable)
