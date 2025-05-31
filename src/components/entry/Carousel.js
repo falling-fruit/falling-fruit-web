26dccd2a03aecf81b8e0df132ea29b67f0c9da73
Clement Raymond <chinexzy37@gmail.com>
2025-05-31 15:50:14 +0000

    Add side dots for carousel navigation in partially open drawer (#605, #733)

--- src/components/entry/Carousel.js
+++ src/components/entry/Carousel.js
@@ -1,10 +1,36 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel'
 import styled from 'styled-components/macro'
 
 import { openLightbox } from '../../redux/locationSlice'
+
+// Styled component for side dots
+const SideDot = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  background: rgba(255, 255, 255, 0.7);
+  cursor: pointer;
+  z-index: 10;
+  transition: background 0.3s;
+  &:hover {
+    background: #fff;
+  }
+`
+
+const LeftDot = styled(SideDot)`
+  left: 10px;
+`
+
+const RightDot = styled(SideDot)`
+  right: 10px;
+`
 
 const Carousel = styled(ResponsiveCarousel)`
   width: 100%;
@@ -24,6 +50,10 @@ const Carousel = styled(ResponsiveCarousel)`
     display: flex;
     width: auto;
     inset-inline-end: 0;
+
+    @media (max-width: 768px) {
+      display: none; /* Hide bottom dots on mobile */
+    }
 
     .dot {
       height: 10px;
@@ -52,6 +82,7 @@ Carousel.defaultProps = {
 const EntryCarousel = () => {
   const dispatch = useDispatch()
   const { reviews } = useSelector((state) => state.location)
+  const [currentSlide, setCurrentSlide] = useState(0)
 
   const reviewsWithPhotos = reviews.filter((review) => review.photos.length > 0)
   const lightboxIndices = reviewsWithPhotos
@@ -61,9 +92,16 @@ const EntryCarousel = () => {
     .map((review) => review.photos)
     .flat()
 
-  const onClickCarousel = (idx) => {
-    const [reviewIndex, photoIndex] = lightboxIndices[idx]
-    dispatch(openLightbox({ reviewIndex, photoIndex }))
+  const handlePrev = () => {
+    setCurrentSlide((prev) =>
+      prev === 0 ? allReviewPhotos.length - 1 : prev - 1,
+    )
+  }
+
+  const handleNext = () => {
+    setCurrentSlide((prev) =>
+      prev === allReviewPhotos.length - 1 ? 0 : prev + 1,
+    )
   }
 
   if (allReviewPhotos.length === 0) {
@@ -71,14 +109,27 @@ const EntryCarousel = () => {
   }
 
   return (
-    <Carousel
-      onClickItem={onClickCarousel}
-      showIndicators={allReviewPhotos.length > 1}
-    >
-      {allReviewPhotos.map((photo) => (
-        <img key={photo.id} src={photo.medium} alt="entry" />
-      ))}
-    </Carousel>
+    <div style={{ position: 'relative' }}>
+      <Carousel
+        selectedItem={currentSlide}
+        onChange={setCurrentSlide}
+        onClickItem={(idx) => {
+          const [reviewIndex, photoIndex] = lightboxIndices[idx]
+          dispatch(openLightbox({ reviewIndex, photoIndex }))
+        }}
+        showIndicators={allReviewPhotos.length > 1}
+      >
+        {allReviewPhotos.map((photo) => (
+          <img key={photo.id} src={photo.medium} alt="entry" />
+        ))}
+      </Carousel>
+      {allReviewPhotos.length > 1 && (
+        <>
+          <LeftDot onClick={handlePrev} />
+          <RightDot onClick={handleNext} />
+        </>
+      )}
+    </div>
   )
 }
 
