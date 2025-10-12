245226644cb119890539b3383ad35203c8ffd569
Marcelo Amorim <mamorim813@gmail.com>
2025-10-12 18:17:20 -0300

    Add DonationButton and DonationModal components (#870)

---
+++ src/components/about/DonationButton.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import styled from 'styled-components/macro'
+
+import DonationModal from './DonationModal'
+
+const PointerButton = styled.button`
+  cursor: pointer;
+`
+
+const DonationButton = () => {
+  const { t } = useTranslation()
+  const [isDonationModalOpen, setIsDonationModalOpen] = useState(false)
+
+  return (
+    <>
+      <DonationModal
+        isOpen={isDonationModalOpen}
+        onDismiss={() => setIsDonationModalOpen(false)}
+      />
+      <PointerButton onClick={() => setIsDonationModalOpen(true)}>
+        {t('pages.about.donate')}
+      </PointerButton>
+    </>
+  )
+}
+
+export default DonationButton
