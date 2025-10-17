a85030d575e0de1833fd46cdffad4d7781e78b20
Marcelo Amorim <mamorim813@gmail.com>
2025-10-17 14:30:48 -0300

    Fix scroll position clearing logic in DisconnectLastViewedListPosition (#540)
     - Added clearLastViewedListPositionId as proper reducer action in listSlice.
     - Only clear scroll position when navigating outside list/location context.
     - Track navigation path to preserve scroll state for list -> location -> back flow.

--- src/components/connect/DisconnectLastViewedListPosition.js
+++ src/components/connect/DisconnectLastViewedListPosition.js
@@ -1,14 +1,28 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useDispatch } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 
 import { clearLastViewedListPositionId } from '../../redux/listSlice'
 
 const DisconnectLastViewedListPosition = () => {
   const dispatch = useDispatch()
+  const location = useLocation()
+  const previousPath = useRef(location.pathname)
+
+  useEffect(() => {
+    previousPath.current = location.pathname
+  }, [location.pathname])
 
   useEffect(
     () => () => {
-      dispatch(clearLastViewedListPositionId)
+      const currentPath = window.location.pathname
+      const isLeavingContext =
+        !currentPath.startsWith('/list') &&
+        !currentPath.startsWith('/locations')
+
+      if (isLeavingContext) {
+        dispatch(clearLastViewedListPositionId())
+      }
     },
     [dispatch],
   )
