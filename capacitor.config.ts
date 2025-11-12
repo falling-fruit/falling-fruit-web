76014686b59d0af1866b6520c91d54f5c1a88d05
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2025-11-12 10:35:37 +0000

    Capacitor initial setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'org.fallingfruit.beta',
+  appName: 'Falling Fruit',
+  webDir: 'build',
+}
+
+export default config
