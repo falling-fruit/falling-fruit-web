77c4a3134fea49db9fc5d3c3399a367d7d491a51
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2025-12-02 13:17:40 +0000

    Break up email settings page
    Also: replace cancel button with back arrow

--- src/components/account/ChangeEmailPage.js
+++ src/components/account/ChangeEmailPage.js
@@ -1,7 +1,9 @@
 import { Form, Formik } from 'formik'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import styled from 'styled-components/macro'
 import * as Yup from 'yup'
 
 import { editProfile } from '../../redux/authSlice'
@@ -12,7 +14,8 @@ import {
   FormButtonWrapper,
   FormInputWrapper,
 } from '../auth/AuthWrappers'
-import { Checkbox, Input } from '../form/FormikWrappers'
+import { Input } from '../form/FormikWrappers'
+import { BackButton } from '../ui/ActionButtons'
 import Button from '../ui/Button'
 import LabeledRow from '../ui/LabeledRow'
 import LoadingIndicator from '../ui/LoadingIndicator'
@@ -27,18 +30,24 @@ const formToUser = ({ email, password, announcements_email, name }, user) => ({
   range: null,
 })
 
+const StyledBackButton = styled(BackButton)`
+  margin-bottom: 23px;
+`
+
 const ChangeEmailPage = () => {
   const dispatch = useDispatch()
   const { user, isLoading } = useSelector((state) => state.auth)
   const isLoggedIn = !!user
   const { t } = useTranslation()
   const history = useAppHistory()
+  const [showEmailForm, setShowEmailForm] = useState(false)
+  const [isUpdatingPreferences, setIsUpdatingPreferences] = useState(false)
 
   if (!isLoggedIn && !isLoading) {
     return <Redirect to={pathWithCurrentView('/auth/sign_in')} />
   }
 
-  const handleSubmit = (values, formikProps) => {
+  const handleEmailSubmit = (values, formikProps) => {
     dispatch(editProfile(formToUser(values, user))).then((action) => {
       if (action.error) {
         formikProps.setSubmitting(false)
@@ -48,93 +57,134 @@ const ChangeEmailPage = () => {
     })
   }
 
+  const handleAnnouncementsToggle = (checked) => {
+    setIsUpdatingPreferences(true)
+    const updatedUser = {
+      email: user.email,
+      announcements_email: checked,
+      name: user.name || null,
+      bio: user.bio || null,
+      range: null,
+    }
+    dispatch(editProfile(updatedUser)).then(() => {
+      setIsUpdatingPreferences(false)
+    })
+  }
+
   return (
     <Page>
+      <StyledBackButton backPath="/account/edit" />
       <h1>{t('users.email_settings')}</h1>
 
       {user ? (
         <>
-          <Formik
-            initialValues={{
-              email: user.email,
-              password: '',
-              announcements_email: user.announcements_email,
-              name: user.name || '',
-            }}
-            validationSchema={Yup.object({
-              email: Yup.string().email().required(),
-              password: Yup.string().when('email', {
-                is: (email) => email !== user.email,
-                then: (schema) =>
-                  schema.required({
+          <div style={{ marginBottom: '24px' }}>
+            <label
+              style={{
+                display: 'block',
+                marginBottom: '8px',
+                fontWeight: 'bold',
+              }}
+            >
+              {t('glossary.email')}
+            </label>
+            <div style={{ padding: '8px 0' }}>{user.email}</div>
+          </div>
+
+          <div style={{ height: '3em', marginBottom: '1.5em' }}>
+            {isUpdatingPreferences ? (
+              <LoadingIndicator />
+            ) : (
+              <LabeledRow
+                label={
+                  <label htmlFor="announcements_email_toggle">
+                    {t('users.options.announcements_email')}
+                  </label>
+                }
+                left={
+                  <input
+                    type="checkbox"
+                    id="announcements_email_toggle"
+                    checked={user.announcements_email}
+                    onChange={(e) =>
+                      handleAnnouncementsToggle(e.target.checked)
+                    }
+                  />
+                }
+              />
+            )}
+          </div>
+
+          {!showEmailForm ? (
+            <div style={{ marginBottom: '1.5em' }}>
+              <Button onClick={() => setShowEmailForm(true)}>
+                {t('users.change_your_email')}
+              </Button>
+            </div>
+          ) : (
+            <>
+              <h3 style={{ marginBottom: '1.5em' }}>
+                {t('users.change_your_email')}
+              </h3>
+              <Formik
+                initialValues={{
+                  email: '',
+                  password: '',
+                  announcements_email: user.announcements_email,
+                  name: user.name || '',
+                }}
+                validationSchema={Yup.object({
+                  email: Yup.string().email().required(),
+                  password: Yup.string().required({
                     key: 'form.error.missing_password',
                   }),
-                otherwise: (schema) => schema,
-              }),
-            })}
-            onSubmit={handleSubmit}
-          >
-            {({ errors, values, initialValues, isValid, isSubmitting }) => {
-              // Only consider the form dirty if email or announcements_email have changed
-              const isDirty =
-                values.email !== initialValues.email ||
-                values.announcements_email !== initialValues.announcements_email
+                })}
+                onSubmit={handleEmailSubmit}
+              >
+                {({ errors, isValid, isSubmitting }) => (
+                  <Form>
+                    <FormInputWrapper>
+                      <Input
+                        name="email"
+                        type="email"
+                        label={t('users.new_email')}
+                        autoComplete="email"
+                      />
+                      {errors.email && (
+                        <ErrorMessage>
+                          {t(errors.email.key, errors.email.options)}
+                        </ErrorMessage>
+                      )}
 
-              return (
-                <Form>
-                  <FormInputWrapper>
-                    <Input
-                      name="email"
-                      type="email"
-                      label={t('glossary.email')}
-                      autoComplete="email"
-                    />
-                    {errors.email && (
-                      <ErrorMessage>
-                        {t(errors.email.key, errors.email.options)}
-                      </ErrorMessage>
-                    )}
-
-                    <Input
-                      name="password"
-                      type="password"
-                      label={t('glossary.password')}
-                      autoComplete="current-password"
-                    />
-                    {errors.password && (
-                      <ErrorMessage>
-                        {t(errors.password.key, errors.password.options)}
-                      </ErrorMessage>
-                    )}
-                  </FormInputWrapper>
-                  <LabeledRow
-                    label={
-                      <label htmlFor="announcements_email">
-                        {t('users.options.announcements_email')}
-                      </label>
-                    }
-                    left={<Checkbox id="announcements_email" />}
-                    style={{ margin: '16px 0 8px 0' }}
-                  />
-                  <FormButtonWrapper>
-                    <Button
-                      secondary
-                      type="button"
-                      onClick={() => history.push('/account/edit')}
-                    >
-                      {t('form.button.cancel')}
-                    </Button>
-                    <Button
-                      type="submit"
-                      disabled={!isDirty || !isValid || isSubmitting}
-                    >
-                      {t('form.button.submit')}
-                    </Button>
-                  </FormButtonWrapper>
-                </Form>
-              )
-            }}
-          </Formik>
+                      <Input
+                        name="password"
+                        type="password"
+                        label={t('glossary.password')}
+                        autoComplete="current-password"
+                      />
+                      {errors.password && (
+                        <ErrorMessage>
+                          {t(errors.password.key, errors.password.options)}
+                        </ErrorMessage>
+                      )}
+                    </FormInputWrapper>
+                    <FormButtonWrapper>
+                      <Button
+                        secondary
+                        type="button"
+                        onClick={() => setShowEmailForm(false)}
+                      >
+                        {t('form.button.cancel')}
+                      </Button>
+                      <Button type="submit" disabled={!isValid || isSubmitting}>
+                        {t('form.button.submit')}
+                      </Button>
+                    </FormButtonWrapper>
+                  </Form>
+                )}
+              </Formik>
+            </>
+          )}
         </>
       ) : (
         <LoadingIndicator vertical cover />
