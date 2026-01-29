fdb49e47f8b50da64618dfec16e602ed89b9f30a
Wojtek Bazant <wojciech.bazant+ebi@gmail.com>
2026-01-29 11:44:34 +0000

    Draw locations with Google maps markers
    Also a few items that showed up in profiling:
    - useShareUrl needed a memo
    - router update on view change was needless

--- src/components/map/MapPage.js
+++ src/components/map/MapPage.js
@@ -22,7 +22,7 @@ import LoadingIndicator from '../ui/LoadingIndicator'
 import CloseStreetView from './CloseStreetView'
 import Cluster from './Cluster'
 import GeolocationDot from './GeolocationDot'
-import Location from './Location'
+import LocationMarkers from './LocationMarkers'
 import PanoramaHandler from './PanoramaHandler'
 import {
   AddLocationCentralUnmovablePin,
@@ -30,6 +30,7 @@ import {
   EditLocationCentralUnmovablePin,
 } from './Pins'
 import Place from './Place'
+import SelectedLocation from './SelectedLocation'
 import TrackLocationButton from './TrackLocationButton'
 
 const MIN_ZOOM = 1
@@ -158,7 +159,7 @@ const makeHandleViewChange = (dispatch, googleMap, history) => {
     }
     throttledDispatches(newView)
     if (googleMapReactCallbackArg) {
-      history.changeView(newView)
+      history.syncViewToBrowserUrl(newView)
     }
   }
 }
@@ -196,6 +197,7 @@ const MapPage = ({ isDesktop }) => {
   const history = useAppHistory()
   const dispatch = useDispatch()
   const handleViewChangeRef = useRef(() => void 0)
+  const mapClickListenerRef = useRef(null)
 
   const [draggedPosition, setDraggedPosition] = useState(null)
   const [shareOpen, setShareOpen] = useState(false)
@@ -338,19 +340,44 @@ const MapPage = ({ isDesktop }) => {
     }
   }
 
-  const handleLocationClick = (location) => {
-    if (isDesktop && pathname.includes('/settings')) {
-      dispatch(setFromSettings(true))
-    }
-    history.push(`/locations/${location.id}`)
-  }
+  const handleLocationClick = useCallback(
+    (location) => {
+      if (isDesktop && pathname.includes('/settings')) {
+        dispatch(setFromSettings(true))
+      }
+      history.push(`/locations/${location.id}`)
+    },
+    [isDesktop, pathname, dispatch, history],
+  )
 
-  const handleNonspecificClick = ({ event }) => {
-    event.stopPropagation()
-    if (isViewingLocation) {
-      history.push('/map')
+  useEffect(() => {
+    if (!googleMap || !getGoogleMaps) {
+      return
     }
-  }
+
+    const google = getGoogleMaps()
+
+    if (mapClickListenerRef.current) {
+      google.event.removeListener(mapClickListenerRef.current)
+    }
+
+    mapClickListenerRef.current = google.event.addListener(
+      googleMap,
+      'click',
+      () => {
+        if (isViewingLocation) {
+          history.push('/map')
+        }
+      },
+    )
+
+    return () => {
+      if (mapClickListenerRef.current) {
+        google.event.removeListener(mapClickListenerRef.current)
+        mapClickListenerRef.current = null
+      }
+    }
+  }, [googleMap, getGoogleMaps, isViewingLocation, history])
 
   const zoomIn = () => {
     googleMap?.setZoom(currentZoom + 1)
@@ -414,7 +441,6 @@ const MapPage = ({ isDesktop }) => {
       {showStreetView && <CloseStreetView />}
       {initialView && (
         <GoogleMapWrapper
-          onClick={handleNonspecificClick}
           bootstrapURLKeys={{
             apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
             version: 'quarterly',
@@ -489,6 +515,14 @@ const MapPage = ({ isDesktop }) => {
               lng={geolocation.longitude}
             />
           )}
+          {locationId && selectedLocation && (
+            <SelectedLocation
+              lat={selectedLocation.lat}
+              lng={selectedLocation.lng}
+              selected={isViewingLocation}
+              editing={isEditingLocation}
+            />
+          )}
           {place &&
             place.location &&
             place.view &&
@@ -512,25 +546,13 @@ const MapPage = ({ isDesktop }) => {
               lng={cluster.lng}
             />
           ))}
-          {allLocations.map((location) => (
-            <Location
-              key={location.id}
-              onClick={
-                isEditingLocation || isAddingLocation
-                  ? null
-                  : (event) => {
-                      handleLocationClick(location)
-                      event.stopPropagation()
-                    }
-              }
-              lat={location.lat}
-              lng={location.lng}
-              selected={location.id === locationId}
-              editing={isEditingLocation && location.id === locationId}
-              showLabel={showLabels}
-              typeIds={location.type_ids}
-            />
-          ))}
+          <LocationMarkers
+            locations={allLocations}
+            googleMap={googleMap}
+            getGoogleMaps={getGoogleMaps}
+            onLocationClick={handleLocationClick}
+            showLabels={showLabels}
+          />
           {(isEditingLocation || isAddingLocation) && draggedPosition && (
             <DraggableMapPin
               lat={draggedPosition.lat}
