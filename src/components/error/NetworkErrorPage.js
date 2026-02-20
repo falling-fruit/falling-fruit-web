cc84b5227de78b228378c019833801379486c60b
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2026-02-20 14:43:51 +0000

    Two buttons and new wording

--- src/components/error/NetworkErrorPage.js
+++ src/components/error/NetworkErrorPage.js
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router-dom'
 
 import { ErrorPage } from '../ui/PageTemplate'
-import ReloadButton from './ReloadButton'
+import RestartAndReloadButtons from './RestartAndReloadButtons'
 
 const NetworkErrorPage = () => {
   const { t } = useTranslation()
@@ -16,7 +16,7 @@ const NetworkErrorPage = () => {
         <h1>{t('pages.network_error.header_message')}</h1>
         <p>{t('pages.network_error.could_not_connect')}</p>
         <p>{t('pages.network_error.please_try_again')}</p>
-        <ReloadButton fromPage={fromPage} />
+        <RestartAndReloadButtons fromPage={fromPage} />
       </div>
     </ErrorPage>
   )
