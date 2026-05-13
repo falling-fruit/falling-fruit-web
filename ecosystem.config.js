e2889bd41f63fd9f1cbd49ae7c36d0f02696e49b
ezwelty <ethan.welty@gmail.com>
2026-05-13 12:40:51 +0200

    Serve with vercel serve instead of pm2 serve
    This allows more configuration, including avoiding redirect to index.html for select files.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,11 @@
 //Knip marks this file as unused, but is still needed for future development purposes.
 module.exports = {
   name: 'web',
-  script: 'serve',
+  script: 'node_modules/.bin/serve',
+  args: '-p 8082 ./build',
   watch: false,
   env: {
     NODE_ENV: 'production',
-    PM2_SERVE_PATH: 'build',
-    PM2_SERVE_PORT: 8082,
-    PM2_SERVE_SPA: 'true',
-    PM2_SERVE_HOMEPAGE: '/index.html',
   },
   exec_mode: 'cluster',
   instances: 4,
