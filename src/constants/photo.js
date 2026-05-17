7d4f13df686d2952c010f86156f0c61e17f736f4
ezwelty <ethan.welty@gmail.com>
2026-05-17 16:50:44 +0200

    Treat photo width and height equally
    Also move JPEG quality and max size to constants.

---
+++ src/constants/photo.js
@@ -0,0 +1,14 @@
+/**
+ * Maximum dimension (width or height) for photos, in pixels.
+ *
+ * Photos are downsized (if needed) while maintaining their aspect ratio.
+ * @constant {number}
+ */
+export const PHOTO_MAX_DIMENSION = 1200
+
+/**
+ * JPEG compression quality for photos, between 0 and 1.
+ *
+ * @constant {number}
+ */
+export const JPEG_QUALITY = 0.8
