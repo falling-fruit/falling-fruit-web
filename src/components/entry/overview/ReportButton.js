c990e02cdba801b0f596f8d71892cfd7bc5db969
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2026-05-19 12:52:50 +0100

    Avoid text boxes in partially open drawer

--- src/components/entry/overview/ReportButton.js
+++ src/components/entry/overview/ReportButton.js
@@ -5,17 +5,26 @@ import { useSelector } from 'react-redux'
 
 import { ReportModal } from '../../form/ReportModal'
 import Button from '../../ui/Button'
+import useLocationPane from '../useLocationPane'
 
 export const ReportButton = () => {
   const { location: locationData } = useSelector((state) => state.location)
   const { t } = useTranslation()
   const typesAccess = useSelector((state) => state.type.typesAccess)
+  const { drawerFullyOpen, fullyOpenPaneDrawer } = useLocationPane()
 
   const locationName = locationData?.type_ids
     .map((id) => typesAccess?.getType(id)?.commonName)
     .filter(Boolean)
     .join(', ')
   const [isReportModalOpen, setIsReportModalOpen] = useState(false)
+
+  const handleReportClick = () => {
+    if (!drawerFullyOpen) {
+      fullyOpenPaneDrawer()
+    }
+    setIsReportModalOpen(true)
+  }
 
   return (
     <>
@@ -27,11 +36,7 @@ export const ReportButton = () => {
           onDismiss={() => setIsReportModalOpen(false)}
         />
       )}
-      <Button
-        leftIcon={<Flag />}
-        secondary
-        onClick={() => setIsReportModalOpen(true)}
-      >
+      <Button leftIcon={<Flag />} secondary onClick={handleReportClick}>
         {t('form.button.report')}
       </Button>
     </>
