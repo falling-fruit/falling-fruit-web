50009e4d08f98a0897e6b6772e038b1fa177412e
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2026-05-19 13:46:03 +0100

    Better photos

--- src/constants/photo.js
+++ src/constants/photo.js
@@ -4,11 +4,11 @@
  * Photos are downsized (if needed) while maintaining their aspect ratio.
  * @constant {number}
  */
-export const PHOTO_MAX_DIMENSION = 1200
+export const PHOTO_MAX_DIMENSION = 2048
 
 /**
  * JPEG compression quality for photos, between 0 and 1.
  *
  * @constant {number}
  */
-export const JPEG_QUALITY = 0.8
+export const JPEG_QUALITY = 0.85
