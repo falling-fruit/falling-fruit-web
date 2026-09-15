b9c5790650701c661b7aa66264f0906e65d6ec9a
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2026-09-15 10:43:42 +0100

    add skeletons for user and dataset page. Remove LoadingOverlay

--- src/components/about/AboutDatasetPage.js
+++ src/components/about/AboutDatasetPage.js
@@ -2,6 +2,7 @@ import { Calendar } from '@styled-icons/boxicons-regular'
 import { Copyright, MapPin, Pin } from '@styled-icons/boxicons-solid'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import Skeleton from 'react-loading-skeleton'
 import { useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
@@ -12,7 +13,6 @@ import { BackButton } from '../ui/ActionButtons'
 import { theme } from '../ui/GlobalStyle'
 import { PageHeader } from '../ui/Headers'
 import IconBesideText from '../ui/IconBesideText'
-import { LoadingOverlay } from '../ui/LoadingIndicator'
 import { Page } from '../ui/PageTemplate'
 
 const AboutDatasetPage = () => {
@@ -47,52 +47,80 @@ const AboutDatasetPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id])
 
-  if (isLoading) {
-    return <LoadingOverlay />
-  }
-
   const { name, url, comments, muni, location_count, created_at, license } =
     importData
 
   return (
     <Page>
       <BackButton />
-      <PageHeader>
-        {t('pages.datasets.import_id_and_name', { id, name })}
-      </PageHeader>
-      <a href={url} target="_blank" rel="noreferrer">
-        {url}
-      </a>
-      <p dir="auto" style={{ whiteSpace: 'pre-line' }}>
-        {comments}
-      </p>
-      <IconBesideText>
-        <Pin color={theme.secondaryText} size={20} />
-        <p>
-          {muni
-            ? t('glossary.tree_inventory.one')
-            : t('pages.datasets.community_map')}
-        </p>
-      </IconBesideText>
-      <IconBesideText>
-        <MapPin color={theme.secondaryText} size={20} />
-        <p>{`${location_count} ${location_count === 1 ? t('glossary.locations.one') : t('glossary.locations.other')}`}</p>
-      </IconBesideText>
-      <IconBesideText>
-        <Calendar color={theme.secondaryText} size={20} />
-        <p>
-          <time dateTime={created_at}>
-            {t('pages.datasets.imported_on', {
-              date: formatISOString(created_at, i18n.language),
-            })}
-          </time>
-        </p>
-      </IconBesideText>
-      {license && (
-        <IconBesideText>
-          <Copyright color={theme.secondaryText} size={20} />
-          <p>{license} </p>
-        </IconBesideText>
+      {isLoading ? (
+        <>
+          <PageHeader>
+            <Skeleton width="14em" />
+          </PageHeader>
+          <Skeleton width="16em" />
+          <p>
+            <Skeleton count={2} width="80%" />
+          </p>
+          <IconBesideText>
+            <Pin color={theme.secondaryText} size={20} />
+            <p>
+              <Skeleton width="8em" />
+            </p>
+          </IconBesideText>
+          <IconBesideText>
+            <MapPin color={theme.secondaryText} size={20} />
+            <p>
+              <Skeleton width="7em" />
+            </p>
+          </IconBesideText>
+          <IconBesideText>
+            <Calendar color={theme.secondaryText} size={20} />
+            <p>
+              <Skeleton width="10em" />
+            </p>
+          </IconBesideText>
+        </>
+      ) : (
+        <>
+          <PageHeader>
+            {t('pages.datasets.import_id_and_name', { id, name })}
+          </PageHeader>
+          <a href={url} target="_blank" rel="noreferrer">
+            {url}
+          </a>
+          <p dir="auto" style={{ whiteSpace: 'pre-line' }}>
+            {comments}
+          </p>
+          <IconBesideText>
+            <Pin color={theme.secondaryText} size={20} />
+            <p>
+              {muni
+                ? t('glossary.tree_inventory.one')
+                : t('pages.datasets.community_map')}
+            </p>
+          </IconBesideText>
+          <IconBesideText>
+            <MapPin color={theme.secondaryText} size={20} />
+            <p>{`${location_count} ${location_count === 1 ? t('glossary.locations.one') : t('glossary.locations.other')}`}</p>
+          </IconBesideText>
+          <IconBesideText>
+            <Calendar color={theme.secondaryText} size={20} />
+            <p>
+              <time dateTime={created_at}>
+                {t('pages.datasets.imported_on', {
+                  date: formatISOString(created_at, i18n.language),
+                })}
+              </time>
+            </p>
+          </IconBesideText>
+          {license && (
+            <IconBesideText>
+              <Copyright color={theme.secondaryText} size={20} />
+              <p>{license} </p>
+            </IconBesideText>
+          )}
+        </>
       )}
     </Page>
   )
