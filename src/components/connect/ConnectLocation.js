8379d8d991054890b22b08c569931302e3a3d680
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2026-09-25 16:15:26 +0100

    Extract a helper

--- src/components/connect/ConnectLocation.js
+++ src/components/connect/ConnectLocation.js
@@ -77,6 +77,11 @@ const ToastContent = () => {
   )
 }
 
+const viewFromLocationCenter = ({ lat, lng }) => ({
+  center: { lat, lng },
+  zoom: 16,
+})
+
 const ConnectLocation = ({
   locationId,
   isBeingEdited,
@@ -126,36 +131,28 @@ const ConnectLocation = ({
       }
 
       if (action.payload && !initialView) {
-        const view = viewFromCurrentUrl() || {
-          center: {
-            lat: action.payload.lat,
-            lng: action.payload.lng,
-          },
-          zoom: 16,
-        }
-        dispatch(setInitialView(view))
+        dispatch(
+          setInitialView(
+            viewFromCurrentUrl() || viewFromLocationCenter(action.payload),
+          ),
+        )
       }
     })
   }, [dispatch, locationId]) //eslint-disable-line
 
   useEffect(() => {
     if (location && !hasInitialView) {
-      const view = viewFromCurrentUrl() || {
-        center: {
-          lat: location.lat,
-          lng: location.lng,
-        },
-        zoom: 16,
-      }
-      dispatch(setInitialView(view))
+      dispatch(
+        setInitialView(
+          viewFromCurrentUrl() || viewFromLocationCenter(location),
+        ),
+      )
     }
   }, [!!location, hasInitialView]) //eslint-disable-line
 
   useEffect(() => {
     if (hasInitialView && !viewFromCurrentUrl() && googleMap && location) {
-      const center = { lat: location.lat, lng: location.lng }
-      const zoom = 16
-      history.replaceView({ center, zoom })
+      history.replaceView(viewFromLocationCenter(location))
     }
   }, [
     dispatch,
